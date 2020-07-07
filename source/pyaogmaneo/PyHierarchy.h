d426659b43e53a7c55edeae9f78eee94de71b2cb
Eric Laukien <ericlaukien@gmail.com>
2020-07-07 14:01:48 -0400

    Tests

--- source/pyaogmaneo/PyHierarchy.h
+++ source/pyaogmaneo/PyHierarchy.h
@@ -261,24 +261,6 @@ public:
         return h.getALayers()[v]->gamma;
     }
 
-    void setATemperature(
-        int v,
-        float temperature
-    ) {
-        assert(h.getALayers()[v] != nullptr);
-        
-        h.getALayers()[v]->temperature = temperature;
-    }
-
-    float getATemperature(
-        int v
-    ) const {
-        assert(h.getALayers()[v] != nullptr);
-        
-        return h.getALayers()[v]->temperature;
-    }
-
-
     friend class PyVisualizer;
 };
 } // namespace pyaon
