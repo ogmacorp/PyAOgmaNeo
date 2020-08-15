e57e95e59f0e23a81c105fa6b9b75918d4dcd6d5
Eric Laukien <ericlaukien@gmail.com>
2020-08-15 13:17:55 -0400

    Updates

--- source/pyaogmaneo/PyHierarchy.h
+++ source/pyaogmaneo/PyHierarchy.h
@@ -197,19 +197,6 @@ public:
         return h.getSCLayer(l).alpha;
     }
 
-    void setSCExplainIters(
-        int l,
-        int explainIters
-    ) {
-        h.getSCLayer(l).explainIters = explainIters;
-    }
-
-    int getSCExplainIters(
-        int l
-    ) const {
-        return h.getSCLayer(l).explainIters;
-    }
-
     void setPAlpha(
         int l,
         int v,
