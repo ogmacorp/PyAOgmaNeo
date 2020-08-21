90105d826bf0f3a4783302cd7c77c4b5eb82ba28
Eric Laukien <ericlaukien@gmail.com>
2020-08-21 12:38:10 -0400

    Added lunar lander

---
+++ examples/LunarLander_EnvRunner.py
@@ -0,0 +1,43 @@
+# ----------------------------------------------------------------------------
+#  PyAOgmaNeo
+#  Copyright(c) 2020 Ogma Intelligent Systems Corp. All rights reserved.
+#
+#  This copy of PyAOgmaNeo is licensed to you under the terms described
+#  in the PYAOGMANEO_LICENSE.md file included in this distribution.
+# ----------------------------------------------------------------------------
+
+# -*- coding: utf-8 -*-
+
+# Simple Cart-Pole example using EnvRunner
+
+import gym
+from EnvRunner import EnvRunner # EnvRunner automatically creates an OgmaNeo2 hierarchy and appropriate encoders for most Gym environments
+
+env = gym.make('LunarLander-v2')
+
+runner = EnvRunner(env, terminalReward=0.0, rewardScale=1.0) # Cart-Pole environment always returns a reward of 1, so use a custom reward function: -1 if episode ends, 0 otherwise
+
+averageReward = 0.0
+maxReward = 0.0
+
+for episode in range(1000):
+    env.reset()
+
+    totalReward = 0.0
+
+    # Timesteps
+    for t in range(10000):
+        done, reward = runner.act() # Step the environment and agent
+
+        totalReward += reward
+
+        if done:
+            if episode == 0:
+                averageReward = totalReward
+                maxReward = totalReward
+            else:
+                averageReward = 0.99 * averageReward + 0.01 * totalReward
+                maxReward = max(maxReward, totalReward)
+
+            print("Episode {} finished after {} timesteps, receiving {} reward. Average: {} Max: {}".format(episode + 1, t + 1, totalReward, averageReward, maxReward))
+            break
