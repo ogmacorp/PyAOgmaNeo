60ac4e15e3c8b7c5a50cb7583d407d27fb82d990
Eric Laukien <ericlaukien@gmail.com>
2020-09-25 19:23:30 -0400

    Added missing accessors

--- examples/WavyLinePrediction.py
+++ examples/WavyLinePrediction.py
@@ -24,7 +24,7 @@ bounds = (-1.0, 1.0)
 # Define layer descriptors: Parameters of each layer upon creation
 lds = []
 
-for i in range(6): # Layers with exponential memory
+for i in range(2): # Layers with exponential memory
     ld = pyaon.LayerDesc()
 
     # Set the hidden (encoder) layer size: width x height x columnSize
@@ -34,7 +34,7 @@ for i in range(6): # Layers with exponential memory
     ld.pRadius = 4 # Predictor radius onto sparse coder hidden layer (and feed back)
 
     ld.ticksPerUpdate = 2 # How many ticks before a layer updates (compared to previous layer) - clock speed for exponential memory
-    ld.temporalHorizon = 2 # Memory horizon of the layer. Must be greater or equal to ticksPerUpdate
+    ld.temporalHorizon = 16 # Memory horizon of the layer. Must be greater or equal to ticksPerUpdate
     
     lds.append(ld)
 
@@ -42,10 +42,10 @@ for i in range(6): # Layers with exponential memory
 h = pyaon.Hierarchy([ pyaon.Int3(1, 1, inputColumnSize) ], [ pyaon.inputTypePrediction ], lds)
 
 # Present the wave sequence for some timesteps
-iters = 3000
+iters = 500
 
 def wave(t):
-    return float(t % 19 == 0)#np.sin(t * 0.02 * 2.0 * np.pi) * np.sin(t * 0.04 * 2.0 * np.pi + 0.45)
+    return float(t % 19 == 0) #np.sin(t * 0.02 * 2.0 * np.pi) * np.sin(t * 0.035 * 2.0 * np.pi + 0.45)
 
 for t in range(iters):
     # The value to encode into the input column
@@ -55,4 +55,45 @@ for t in range(iters):
 
     # Step the hierarchy given the inputs (just one here)
     h.step([ [ valueToEncodeBinned ] ], True) # True for enabling learning
+
+    #print(h.getHiddenCs(0))
+
+    # Print progress
+    if t % 100 == 0:
+        print(t)
+
+# Recall the sequence
+ts = [] # Time step
+vs = [] # Predicted value
+trgs = [] # True value
+
+for t2 in range(500):
+    t = t2 + iters # Continue where previous sequence left off
+
+    # New, continued value for comparison to what the hierarchy predicts
+    valueToEncode = wave(t) # Some wavy line
+
+    # Bin the value into the column and write into the input buffer. We are simply rounding to the nearest integer location to "bin" the scalar into the column
+    valueToEncodeBinned = int((valueToEncode - bounds[0]) / (bounds[1] - bounds[0]) * (inputColumnSize - 1) + 0.5)
+
+    # Run off of own predictions with learning disabled
+    h.step([ h.getPredictionCs(0) ], False) # Learning disabled
+
+    predIndex = h.getPredictionCs(0)[0] # First (only in this case) input layer prediction
+    
+    # Decode value (de-bin)
+    value = predIndex / float(inputColumnSize - 1) * (bounds[1] - bounds[0]) + bounds[0]
+
+    # Append to plot data
+    ts.append(t2)
+    vs.append(value)
+    trgs.append(valueToEncode)
+
+    # Show predicted value
+    #print(value)
+
+# Show plot
+plt.plot(ts, vs, ts, trgs)
+plt.show()
+
 
