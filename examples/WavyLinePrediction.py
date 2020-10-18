abd574606d988f225f67403e7b8e065fc31aafe5
Eric Laukien <ericlaukien@gmail.com>
2020-10-17 22:22:18 -0400

    Tests

--- examples/WavyLinePrediction.py
+++ examples/WavyLinePrediction.py
@@ -28,13 +28,13 @@ for i in range(7): # Layers with exponential memory
     ld = pyaon.LayerDesc()
 
     # Set the hidden (encoder) layer size: width x height x columnSize
-    ld.hiddenSize = pyaon.Int3(4, 4, 16)
+    ld.hiddenSize = pyaon.Int3(3, 3, 16)
 
     ld.ffRadius = 4 # Sparse coder radius onto visible layers
     ld.pRadius = 4 # Predictor radius onto sparse coder hidden layer (and feed back)
 
     ld.ticksPerUpdate = 2 # How many ticks before a layer updates (compared to previous layer) - clock speed for exponential memory
-    ld.temporalHorizon = 4 # Memory horizon of the layer. Must be greater or equal to ticksPerUpdate
+    ld.temporalHorizon = 2 # Memory horizon of the layer. Must be greater or equal to ticksPerUpdate
     
     lds.append(ld)
 
@@ -42,7 +42,7 @@ for i in range(7): # Layers with exponential memory
 h = pyaon.Hierarchy([ pyaon.Int3(1, 1, inputColumnSize) ], [ pyaon.inputTypePrediction ], lds)
 
 # Present the wave sequence for some timesteps
-iters = 30000
+iters = 40000
 
 def wave(t):
     return float(t % 21 == 0)#np.sin(t * 0.01 * 2.0 * np.pi - 0.5) * np.sin(t * 0.04 * 2.0 * np.pi + 0.5)
