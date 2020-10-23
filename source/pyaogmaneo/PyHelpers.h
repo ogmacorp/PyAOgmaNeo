9505bf16597511ad10c4a0da154ff8b9903c3043
Eric Laukien <ericlaukien@gmail.com>
2020-10-23 13:00:24 -0400

    File rename

---
+++ source/pyaogmaneo/PyHelpers.h
@@ -0,0 +1,62 @@
+// ----------------------------------------------------------------------------
+//  PyAOgmaNeo
+//  Copyright(c) 2020 Ogma Intelligent Systems Corp. All rights reserved.
+//
+//  This copy of PyAOgmaNeo is licensed to you under the terms described
+//  in the PYAOGMANEO_LICENSE.md file included in this distribution.
+// ----------------------------------------------------------------------------
+
+#pragma once
+
+#include <aogmaneo/Helpers.h>
+#include <vector>
+#include <fstream>
+
+namespace pyaon {
+class PyStreamReader : public aon::StreamReader {
+public:
+    std::ifstream ins;
+
+    void read(
+        void* data,
+        int len
+    ) override;
+};
+
+class PyStreamWriter : public aon::StreamWriter {
+public:
+    std::ofstream outs;
+
+    void write(
+        const void* data,
+        int len
+    ) override;
+};
+
+class PyBufferReader : public aon::StreamReader {
+public:
+    int start;
+    const std::vector<unsigned char>* buffer;
+
+    PyBufferReader()
+    :
+    start(0),
+    buffer(nullptr)
+    {}
+
+    void read(
+        void* data,
+        int len
+    ) override;
+};
+
+class PyBufferWriter : public aon::StreamWriter {
+public:
+    std::vector<unsigned char> buffer;
+
+    void write(
+        const void* data,
+        int len
+    ) override;
+};
+} // namespace pyaon
