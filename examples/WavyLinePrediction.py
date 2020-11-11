af3e3625430673b6dc4c3c3cbeb0cf38a3c666ae
Eric Laukien <ericlaukien@gmail.com>
2020-11-11 12:36:19 -0500

    Tests

--- examples/WavyLinePrediction.py
+++ examples/WavyLinePrediction.py
@@ -24,11 +24,11 @@ bounds = (-1.0, 1.0)
 # Define layer descriptors: Parameters of each layer upon creation
 lds = []
 
-for i in range(7): # Layers with exponential memory
+for i in range(5): # Layers with exponential memory
     ld = pyaon.LayerDesc()
 
     ld.hiddenSize = (4, 4, 16)
-    
+
     lds.append(ld)
 
 # Create the hierarchy: Provided with input layer sizes (a single column in this case), and input types (a single predicted layer)
@@ -39,10 +39,10 @@ for i in range(len(lds)):
     h.setSCAlpha(i, 0.001)
 
 # Present the wave sequence for some timesteps
-iters = 30000
+iters = 50000
 
 def wave(t):
-    return float(t % 31 == 0)#np.sin(t * 0.01 * 2.0 * np.pi - 0.5) * np.sin(t * 0.04 * 2.0 * np.pi + 0.5)
+    return float(t % 21 == 0)#np.sin(t * 0.01 * 2.0 * np.pi - 0.5) * np.sin(t * 0.04 * 2.0 * np.pi + 0.5)
 
 for t in range(iters):
     # The value to encode into the input column
@@ -53,7 +53,7 @@ for t in range(iters):
     # Step the hierarchy given the inputs (just one here)
     h.step([ [ valueToEncodeBinned ] ], True) # True for enabling learning
 
-    print(h.getHiddenCIs(3))
+    print(h.getHiddenCIs(2))
 
     # Print progress
     if t % 100 == 0:
