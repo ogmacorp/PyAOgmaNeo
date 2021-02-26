b585e2ad2c2620c51d4de4c7b992ae2e5e3f3360
Eric Laukien <ericlaukien@gmail.com>
2021-02-26 14:54:52 -0500

    Added biases back

--- examples/WavyLinePrediction.py
+++ examples/WavyLinePrediction.py
@@ -85,7 +85,7 @@ h = pyaon.Hierarchy()
 h.initRandom([ pyaon.IODesc(size=(2, 4, 16), type=pyaon.prediction, ffRadius=4) ], lds)
 
 # Present the wave sequence for some timesteps
-iters = 50000
+iters = 100000
 
 def wave(t):
     #return np.sin(t  * 2.0 * np.pi * 0.01 + 0.3)
