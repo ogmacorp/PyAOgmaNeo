003eaeb71674c74cb912417d414f71b5de8e3942
Eric Laukien <ericlaukien@gmail.com>
2021-06-25 15:31:35 -0400

    Experiments

--- examples/WavyLinePrediction.py
+++ examples/WavyLinePrediction.py
@@ -1,6 +1,6 @@
 # ----------------------------------------------------------------------------
 #  PyAOgmaNeo
-#  Copyright(c) 2020-2021 Ogma Intelligent Systems Corp. All rights reserved.
+#  Copyright(c) 2020 Ogma Intelligent Systems Corp. All rights reserved.
 #
 #  This copy of PyAOgmaNeo is licensed to you under the terms described
 #  in the PYAOGMANEO_LICENSE.md file included in this distribution.
@@ -45,19 +45,29 @@ def CSDRToF(csdr, cells_per_column, scale_factor=0.25):
 
     return x
 
-def Unorm8ToCSDR(x : float):
-    assert(x >= 0.0 and x <= 1.0)
+# Converts an IEEE float to 8 columns with 16 cells each
+def IEEEToCSDR(x : float):
+    b = struct.pack("<f", x)
 
-    i = int(x * 255.0 + 0.5) & 0xff
+    csdr = []
 
-    return [ int(i & 0x0f), int((i & 0xf0) >> 4) ]
+    for i in range(4):
+        csdr.append(b[i] & 0x0f)
+        csdr.append((b[i] & 0xf0) >> 4)
+
+    return csdr
 
 # Reverse transform of IEEEToCSDR
-def CSDRToUnorm8(csdr):
-    return (csdr[0] | (csdr[1] << 4)) / 255.0
+def CSDRToIEEE(csdr):
+    bs = []
+
+    for i in range(4):
+        bs.append(csdr[i * 2 + 0] | (csdr[i * 2 + 1] << 4))
+
+    return struct.unpack("<f", bytes(bs)) 
 
 # This defines the resolution of the input encoding - we are using a simple single column that represents a bounded scalar through a one-hot encoding. This value is the number of "bins"
-numInputColumns = 2
+numInputColumns = 6
 inputColumnSize = 16
 
 # Define layer descriptors: Parameters of each layer upon creation
@@ -66,16 +76,16 @@ lds = []
 for i in range(8): # Layers with exponential memory
     ld = pyaon.LayerDesc()
 
-    ld.hiddenSize = (4, 4, 32) # Size of the encoder (SparseCoder)
+    ld.hiddenSize = (4, 4, 16) # Size of the encoder (SparseCoder)
 
     lds.append(ld)
 
 # Create the hierarchy
 h = pyaon.Hierarchy()
-h.initRandom([ pyaon.IODesc(size=(1, 2, 16), type=pyaon.prediction) ], lds)
+h.initRandom([ pyaon.IODesc(size=(2, 4, 16), type=pyaon.prediction) ], lds)
 
 # Present the wave sequence for some timesteps
-iters = 5000
+iters = 50000
 
 def wave(t):
     return (np.sin(t * 0.05 * 2.0 * np.pi + 0.5) * np.sin(t * 0.1 * 2.0 * np.pi - 0.5) * np.sin(t * 0.02 * 2.0 * np.pi)) * 0.5 + 0.5
@@ -85,10 +95,12 @@ for t in range(iters):
     valueToEncode = wave(t) # Some wavy line
 
     #csdr = fToCSDR(valueToEncode, numInputColumns, inputColumnSize)
-    csdr = Unorm8ToCSDR(float(valueToEncode))
+    csdr = IEEEToCSDR(float(valueToEncode))
 
     # Step the hierarchy given the inputs (just one here)
     h.step([ csdr ], True) # True for enabling learning
+
+    print(h.getHiddenCIs(4))
 
     # Print progress
     if t % 100 == 0:
@@ -111,7 +123,7 @@ for t2 in range(3000):
 
     # Decode value (de-bin)
     #value = CSDRToF(h.getPredictionCIs(0), inputColumnSize) * maxRange
-    value = CSDRToUnorm8(h.getPredictionCIs(0))
+    value = CSDRToIEEE(h.getPredictionCIs(0))
 
     # Append to plot data
     ts.append(t2)
