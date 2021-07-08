fab428a794096c5bf128fe2d8e61afdd7b81906c
Eric Laukien <ericlaukien@gmail.com>
2021-07-07 20:43:24 -0400

    Tuning

--- examples/EnvRunner.py
+++ examples/EnvRunner.py
@@ -36,7 +36,7 @@ def dense2csdr(d, dimz):
     #return csdr
 
 class EnvRunner:
-    def __init__(self, env, layerSizes=3 * [ (5, 5, 16) ], layerRadius=2, hiddenSize=(8, 8, 16), imageRadius=8, imageScale=1.0, obsResolution=32, actionResolution=16, rewardScale=1.0, terminalReward=0.0, infSensitivity=1.0, nThreads=8):
+    def __init__(self, env, layerSizes=4 * [ (5, 5, 16) ], layerRadius=2, hiddenSize=(8, 8, 16), imageRadius=8, imageScale=1.0, obsResolution=32, actionResolution=16, rewardScale=1.0, terminalReward=0.0, infSensitivity=1.0, nThreads=8):
         self.env = env
 
         pyaon.setNumThreads(nThreads)
@@ -146,7 +146,7 @@ class EnvRunner:
 
         lds = []
 
-        histCap = 4
+        histCap = 8
 
         for i in range(len(layerSizes)):
             ld = pyaon.LayerDesc(hiddenSize=layerSizes[i])
@@ -195,7 +195,7 @@ class EnvRunner:
 
         self.totalR = 0.0
         self.g = 1.0
-        self.goalLR = 0.01
+        self.goalLR = 0.03
         self.gamma = 0.99
         self.traceDecay = 0.98
         self.qPrev = 0.0
