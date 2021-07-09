69ef1196dd409ee7762f54640521270726854d92
Eric Laukien <ericlaukien@gmail.com>
2021-07-09 12:35:32 -0400

    Tests

--- examples/EnvRunner.py
+++ examples/EnvRunner.py
@@ -195,7 +195,7 @@ class EnvRunner:
 
         self.totalR = 0.0
         self.g = 1.0
-        self.goalLR = 0.1
+        self.goalLR = 0.05
         self.gamma = 0.99
         self.traceDecay = 0.98
         self.qPrev = 0.0
@@ -298,14 +298,14 @@ class EnvRunner:
 
             q = np.dot(self.weights, d) / len(self.h.getHiddenCIs(self.h.getNumLayers() - 1))
 
-            tdError = self.totalR + self.gamma * q - self.qPrev
-            #targetQ = self.totalR + self.gamma * q
+            #tdError = self.totalR + self.gamma * q - self.qPrev
+            targetQ = self.totalR + self.gamma * q
 
             self.totalR = 0.0
             self.g = 1.0
 
-            self.weights += self.goalLR * tdError * self.traces
-            #self.weights += self.goalLR * (targetQ - self.weights) * self.traces
+            #self.weights += self.goalLR * tdError * self.traces
+            self.weights += self.goalLR * (targetQ - self.weights) * self.traces
 
             self.traces = np.maximum(self.traces * self.traceDecay, d.reshape((1, -1)))
 
