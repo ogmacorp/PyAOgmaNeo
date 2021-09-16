ba85d246fcd4a9f258fb86935e93967c38368012
Eric Laukien <ericlaukien@gmail.com>
2021-09-16 11:58:14 -0400

    Merged

--- examples/EnvRunner.py
+++ examples/EnvRunner.py
@@ -223,7 +223,7 @@ class EnvRunner:
 
                 self.inputs.append(indices)
 
-    def act(self, epsilon=0.1, obsPreprocess=None):
+    def act(self, epsilon=0.03, obsPreprocess=None):
         feedActions = []
 
         for i in range(len(self.actionIndices)):
