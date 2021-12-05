491a7645557fa1f41432b5612612d2284b4b6644
Eric Laukien <ericlaukien@gmail.com>
2021-12-05 10:00:30 -0500

    Impl

--- source/pyaogmaneo/PyHierarchy.cpp
+++ source/pyaogmaneo/PyHierarchy.cpp
@@ -74,11 +74,6 @@ bool LayerDesc::checkInRange() const {
         allInRange = false;
     }
 
-    if (lRadius < 0) {
-        std::cerr << "Error: lRadius < 0 is not allowed!" << std::endl;
-        allInRange = false;
-    }
-
     if (dRadius < 0) {
         std::cerr << "Error: dRadius < 0 is not allowed!" << std::endl;
         allInRange = false;
@@ -153,7 +148,6 @@ void Hierarchy::initRandom(
         cLayerDescs[l] = aon::Hierarchy::LayerDesc(
             aon::Int3(std::get<0>(layerDescs[l].hiddenSize), std::get<1>(layerDescs[l].hiddenSize), std::get<2>(layerDescs[l].hiddenSize)),
             layerDescs[l].eRadius,
-            layerDescs[l].lRadius,
             layerDescs[l].dRadius,
             layerDescs[l].ticksPerUpdate,
             layerDescs[l].temporalHorizon,
