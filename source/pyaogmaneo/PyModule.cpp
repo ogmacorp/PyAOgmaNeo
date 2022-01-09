da77c1f85cd0e335c69a0339f72391a69a853dd6
Eric Laukien <ericlaukien@gmail.com>
2022-01-08 21:56:46 -0500

    Fastw

--- source/pyaogmaneo/PyModule.cpp
+++ source/pyaogmaneo/PyModule.cpp
@@ -99,6 +99,8 @@ PYBIND11_MODULE(pyaogmaneo, m) {
         .def("getIOSize", &pyaon::Hierarchy::getIOSize)
         .def("setELR", &pyaon::Hierarchy::setELR)
         .def("getELR", &pyaon::Hierarchy::getELR)
+        .def("setEBR", &pyaon::Hierarchy::setEBR)
+        .def("getEBR", &pyaon::Hierarchy::getEBR)
         .def("setDLR", &pyaon::Hierarchy::setDLR)
         .def("getDLR", &pyaon::Hierarchy::getDLR)
         .def("setAVLR", &pyaon::Hierarchy::setAVLR)
