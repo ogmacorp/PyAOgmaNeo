faca2068a742fe17e49fb18f2063fb06a40ecdaa
Eric Laukien <ericlaukien@gmail.com>
2022-02-07 11:36:51 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -95,7 +95,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name="pyaogmaneo",
-    version="1.5.2",
+    version="1.5.3",
     description="Python bindings for the AOgmaNeo library",
     long_description='https://github.com/ogmacorp/PyAOgmaNeo',
     author='Ogma Intelligent Systems Corp',
