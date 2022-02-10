755488fc268975375cb9f15c2cdb4955ea5b30df
Eric Laukien <ericlaukien@gmail.com>
2022-02-10 11:22:36 -0500

    New image encoder

--- source/pyaogmaneo/PyModule.cpp
+++ source/pyaogmaneo/PyModule.cpp
@@ -150,6 +150,6 @@ PYBIND11_MODULE(pyaogmaneo, m) {
         .def("getVisibleSize", &pyaon::ImageEncoder::getVisibleSize)
         .def("setLR", &pyaon::ImageEncoder::setLR)
         .def("getLR", &pyaon::ImageEncoder::getLR)
-        .def("setFalloff", &pyaon::ImageEncoder::setFalloff)
-        .def("getFalloff", &pyaon::ImageEncoder::getFalloff);
+        .def("setMR", &pyaon::ImageEncoder::setMR)
+        .def("getMR", &pyaon::ImageEncoder::getMR);
 }
