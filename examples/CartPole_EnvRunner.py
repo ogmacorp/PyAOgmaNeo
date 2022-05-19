d44214260f5f2c669ad971eed8eb11ba5df27f09
Eric Laukien <ericlaukien@gmail.com>
2022-05-19 11:03:03 -0400

    Impl

--- examples/CartPole_EnvRunner.py
+++ examples/CartPole_EnvRunner.py
@@ -15,14 +15,14 @@ from EnvRunner import EnvRunner # EnvRunner automatically creates an AOgmaNeo hi
 
 env = gym.make('CartPole-v1')
 
-runner = EnvRunner(env, terminalReward=-1.0, rewardScale=0.0) # Cart-Pole environment always returns a reward of 1, so use a custom reward function: -1 if episode ends, 0 otherwise
+runner = EnvRunner(env, terminalReward=1.0, rewardScale=0.0) # Cart-Pole environment always returns a reward of 1, so use a custom reward function: -1 if episode ends, 0 otherwise
 
 for episode in range(10000):
     env.reset()
 
     # Timesteps
     for t in range(500):
-        done, _ = runner.act() # Step the environment and agent
+        done, _ = runner.act(pow(0.99, episode)) # Step the environment and agent
 
         if done:
             print("Episode {} finished after {} timesteps".format(episode + 1, t + 1))
