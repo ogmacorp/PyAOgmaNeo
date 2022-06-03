e15e0b31bfc2754e71de6d2d1814e18a2217fefc
Eric Laukien <ericlaukien@gmail.com>
2022-06-03 10:57:27 -0400

    Pruning3

--- examples/WavyLinePrediction.py
+++ examples/WavyLinePrediction.py
@@ -47,10 +47,13 @@ h = neo.Hierarchy()
 h.initRandom([ neo.IODesc(size=(1, 2, 16), type=neo.prediction) ], lds)
 
 # Present the wave sequence for some timesteps
-iters = 1000
+iters = 10000
 
 # The function we are modeling
 def wave(t):
+    if t % 30 == 0:
+        return 1.0
+    return 0.0
     return (np.sin(t * 0.05 * 2.0 * np.pi + 0.5)) * 0.5 + 0.5
 
 for t in range(iters):
@@ -62,6 +65,7 @@ for t in range(iters):
 
     # Step the hierarchy given the inputs (just one here)
     h.step([ csdr ], True) # True for enabling learning
+    print(h.getHiddenCIs(0))
 
     # Print progress
     if t % 100 == 0:
