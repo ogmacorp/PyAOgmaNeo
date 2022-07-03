c120b0e6086689239a993d7a48475a87db2f3257
Eric Laukien <ericlaukien@gmail.com>
2022-07-03 12:20:39 -0400

    Tests

--- examples/WavyLinePrediction.py
+++ examples/WavyLinePrediction.py
@@ -50,11 +50,11 @@ h = neo.Hierarchy()
 h.initRandom([ neo.IODesc(size=(1, 2, 16), type=neo.prediction) ], lds)
 
 # Present the wave sequence for some timesteps
-iters = 10000
+iters = 7000
 
 # The function we are modeling
 def wave(t):
-    if t % 40 == 0:
+    if t % 50 == 0:
         return 1.0
     return 0.0
     return (np.sin(t * 0.05 * 2.0 * np.pi + 0.5)) * 0.5 + 0.5
