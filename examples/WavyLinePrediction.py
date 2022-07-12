886d365c15316b1bd46edda0de7ef4b693ee2acb
Eric Laukien <ericlaukien@gmail.com>
2022-07-11 20:29:01 -0400

    Rates

--- examples/WavyLinePrediction.py
+++ examples/WavyLinePrediction.py
@@ -37,9 +37,7 @@ lds = []
 for i in range(2): # Layers with exponential memory
     ld = neo.LayerDesc()
 
-    ld.hiddenSize = (4, 4, 32) # Size of the encoder (SparseCoder)
-
-    ld.rRadius = 0
+    ld.hiddenSize = (4, 4, 64) # Size of the encoder (SparseCoder)
 
     lds.append(ld)
 
@@ -48,7 +46,7 @@ h = neo.Hierarchy()
 h.initRandom([ neo.IODesc(size=(1, 2, 16), type=neo.prediction) ], lds)
 
 # Present the wave sequence for some timesteps
-iters = 5000
+iters = 10000
 
 def wave(t):
     if t % 20 == 0 or t % 7 == 0:
