71d59d88340bb1e2b9b8f4bda7376988cd0b190e
Eric Laukien <ericlaukien@gmail.com>
2022-07-19 20:47:22 -0400

    Impl

--- examples/EnvRunner.py
+++ examples/EnvRunner.py
@@ -202,7 +202,7 @@ class EnvRunner:
                 for j in range(len(self.inputLows[i])):
                     if self.inputLows[i][j] < self.inputHighs[i][j]:
                         # Rescale
-                        indices.append(int(min(1.0, max(0.0, (obs[j] - self.inputLows[i][j]) / (self.inputHighs[i][j] - self.inputLows[i][j]))) * (self.inputSizes[i][2] - 1) + 0.5))
+                        indices.append(int((obs[j] - self.inputLows[i][j]) / (self.inputHighs[i][j] - self.inputLows[i][j]) * (self.inputSizes[i][2] - 1) + 0.5))
                         #v = obs[j]
                         #indices.append(int(sigmoid(v * self.infSensitivity) * (self.inputSizes[i][2] - 1) + 0.5))
                     elif self.inputLows[i][j] > self.inputHighs[i][j]: # Inf
@@ -218,7 +218,7 @@ class EnvRunner:
 
                 self.inputs.append(indices)
 
-    def act(self, epsilon=0.01, obsPreprocess=None):
+    def act(self, epsilon=0.02, obsPreprocess=None):
         feedActions = []
 
         for i in range(len(self.actionIndices)):
