e21ab9e8bce7e000f816193a10aa005206ddff57
Eric Laukien <ericlaukien@gmail.com>
2022-08-08 12:30:30 -0400

    Tests

--- examples/WavyLinePrediction.py
+++ examples/WavyLinePrediction.py
@@ -34,7 +34,7 @@ inputColumnSize = 16
 # Define layer descriptors: Parameters of each layer upon creation
 lds = []
 
-for i in range(6): # Layers with exponential memory
+for i in range(8): # Layers with exponential memory
     ld = neo.LayerDesc()
 
     ld.hiddenSize = (4, 4, 16) # Size of the encoder (SparseCoder)
@@ -48,10 +48,13 @@ h = neo.Hierarchy()
 h.initRandom([ neo.IODesc(size=(1, 2, 16), type=neo.prediction) ], lds)
 
 # Present the wave sequence for some timesteps
-iters = 4000
+iters = 50000
 
 def wave(t):
-    return min(1.0, max(0.0, (np.sin(t * 0.05 * 2.0 * np.pi + 0.5)) * 0.5 + 0.5 + np.random.randn() * 0.1))
+    if t % 30 == 0:
+        return 1.0
+    return 0.0
+    return min(1.0, max(0.0, (np.sin(t * 0.05 * 2.0 * np.pi + 0.5)) * np.sin(t * 0.04 * 2.0 * np.pi - 0.4) * 0.5 + 0.5 + np.random.randn() * 0.1))
 
 for t in range(iters):
     # The value to encode into the input column
@@ -73,7 +76,7 @@ vs = [] # Predicted value
 
 trgs = [] # True value
 
-for t2 in range(200):
+for t2 in range(500):
     t = t2 + iters # Continue where previous sequence left off
 
     # New, continued value for comparison to what the hierarchy predicts
