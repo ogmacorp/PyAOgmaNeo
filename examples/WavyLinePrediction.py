9c62ee985efaa13a6db06267b312d4bc25b37537
Eric Laukien <ericlaukien@gmail.com>
2022-08-12 20:35:44 -0400

    Tuning

--- examples/WavyLinePrediction.py
+++ examples/WavyLinePrediction.py
@@ -51,7 +51,7 @@ h.initRandom([ neo.IODesc(size=(1, 2, 16), type=neo.prediction) ], lds)
 iters = 10000
 
 def wave(t):
-    if t % 30 == 0:
+    if t % 20 == 0:
         return 1.0
     return 0.0
     return min(1.0, max(0.0, (np.sin(t * 0.05 * 2.0 * np.pi + 0.5)) * np.sin(t * 0.04 * 2.0 * np.pi - 0.4) * 0.5 + 0.5 + np.random.randn() * 0.1))
@@ -65,7 +65,7 @@ for t in range(iters):
     # Step the hierarchy given the inputs (just one here)
     h.step([ csdr ], True) # True for enabling learning
 
-    print(h.getHiddenActs(0))
+    #print(h.getHiddenActs(0))
 
     # Print progress
     if t % 100 == 0:
