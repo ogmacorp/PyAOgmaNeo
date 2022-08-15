e87dc1368729ccd1bf7c2e5e722fb37db01e23fe
Eric Laukien <ericlaukien@gmail.com>
2022-08-14 23:22:48 -0400

    Topo

--- examples/WavyLinePrediction.py
+++ examples/WavyLinePrediction.py
@@ -1,6 +1,6 @@
 # ----------------------------------------------------------------------------
 #  PyAOgmaNeo
-#  Copyright(c) 2020-2021 Ogma Intelligent Systems Corp. All rights reserved.
+#  Copyright(c) 2020-2022 Ogma Intelligent Systems Corp. All rights reserved.
 #
 #  This copy of PyAOgmaNeo is licensed to you under the terms described
 #  in the PYAOGMANEO_LICENSE.md file included in this distribution.
@@ -8,42 +8,13 @@
 
 # -*- coding: utf-8 -*-
 
-import pyaogmaneo as pyaon
+import pyaogmaneo as neo
 import numpy as np
 import matplotlib.pyplot as plt
 import struct
 
 # Set the number of threads
-pyaon.setNumThreads(8)
-
-def fToCSDR(x, num_columns, cells_per_column, scale_factor=0.25):
-    csdr = []
-
-    scale = 1.0
-
-    for i in range(num_columns):
-        s = (x / scale) % (1.0 if x > 0.0 else -1.0)
-
-        csdr.append(int((s * 0.5 + 0.5) * (cells_per_column - 1) + 0.5))
-
-        rec = scale * (float(csdr[i]) / float(cells_per_column - 1) * 2.0 - 1.0)
-        x -= rec
-
-        scale *= scale_factor
-
-    return csdr
-
-def CSDRToF(csdr, cells_per_column, scale_factor=0.25):
-    x = 0.0
-
-    scale = 1.0
-
-    for i in range(len(csdr)):
-        x += scale * (float(csdr[i]) / float(cells_per_column - 1) * 2.0 - 1.0)
-
-        scale *= scale_factor
-
-    return x
+neo.setNumThreads(4)
 
 def Unorm8ToCSDR(x : float):
     assert(x >= 0.0 and x <= 1.0)
@@ -63,33 +34,33 @@ inputColumnSize = 16
 # Define layer descriptors: Parameters of each layer upon creation
 lds = []
 
-for i in range(8): # Layers with exponential memory
-    ld = pyaon.LayerDesc()
+for i in range(8): # LayFrs with exponential memory
+    ld = neo.LayerDesc()
 
     ld.hiddenSize = (4, 4, 32) # Size of the encoder (SparseCoder)
 
     lds.append(ld)
 
 # Create the hierarchy
-h = pyaon.Hierarchy()
-h.initRandom([ pyaon.IODesc(size=(1, 2, 16), type=pyaon.prediction) ], lds)
+h = neo.Hierarchy()
+h.initRandom([ neo.IODesc(size=(1, 2, 16), type=neo.prediction) ], lds)
 
 # Present the wave sequence for some timesteps
-iters = 10000
+iters = 50000
 
 def wave(t):
-    return (np.sin(t * 0.05 * 2.0 * np.pi + 0.5) * np.sin(t * 0.1 * 2.0 * np.pi - 0.5) * np.sin(t * 0.02 * 2.0 * np.pi)) * 0.5 + 0.5
+    return min(1.0, max(0.0, (np.sin(t * 0.05 * 2.0 * np.pi + 0.5)) * 0.5 + 0.5 + np.random.randn() * 0.1))
 
 for t in range(iters):
     # The value to encode into the input column
     valueToEncode = wave(t) # Some wavy line
 
-    #csdr = fToCSDR(valueToEncode, numInputColumns, inputColumnSize)
     csdr = Unorm8ToCSDR(float(valueToEncode))
 
     # Step the hierarchy given the inputs (just one here)
     h.step([ csdr ], True) # True for enabling learning
 
+    print(h.getHiddenCIs(0))
     # Print progress
     if t % 100 == 0:
         print(t)
@@ -100,7 +71,7 @@ vs = [] # Predicted value
 
 trgs = [] # True value
 
-for t2 in range(3000):
+for t2 in range(500):
     t = t2 + iters # Continue where previous sequence left off
 
     # New, continued value for comparison to what the hierarchy predicts
@@ -109,9 +80,7 @@ for t2 in range(3000):
     # Run off of own predictions with learning disabled
     h.step([ h.getPredictionCIs(0) ], False) # Learning disabled
 
-    print(h.getHiddenCIs(0))
     # Decode value (de-bin)
-    #value = CSDRToF(h.getPredictionCIs(0), inputColumnSize) * maxRange
     value = CSDRToUnorm8(h.getPredictionCIs(0))
 
     # Append to plot data
@@ -125,9 +94,6 @@ for t2 in range(3000):
 
 # Show plot
 plt.plot(ts, vs, ts, trgs)
-
-#for i in range(len(units)):
-#    plt.plot(ts, units[i])
 
 plt.show()
 
