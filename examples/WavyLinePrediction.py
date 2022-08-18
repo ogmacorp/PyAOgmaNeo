187813c88a1df183c5679bfd742d64c42b0e5963
Eric Laukien <ericlaukien@gmail.com>
2022-08-18 17:03:59 -0400

    Tests

--- examples/WavyLinePrediction.py
+++ examples/WavyLinePrediction.py
@@ -46,27 +46,20 @@ h = neo.Hierarchy()
 h.initRandom([ neo.IODesc(size=(1, numInputColumns, inputColumnSize), type=neo.prediction, dRadius=4) ], lds)
 
 # Present the wave sequence for some timesteps
-iters = 50000
+iters = 100000
 
 def wave(t):
-    return min(1.0, max(0.0, (np.sin(t * 0.05 * 2.0 * np.pi + 0.5)) * np.sin(t * 0.04 * 2.0 * np.pi - 0.4) * 0.5 + 0.5 + np.random.randn() * 0.02))
-
-t2 = 0
+    return min(1.0, max(0.0, (np.sin(t * 0.05 * 2.0 * np.pi + 0.5)) * np.sin(t * 0.04 * 2.0 * np.pi - 0.4) * 0.5 + 0.5 + np.random.randn() * 0.05))
 
 for t in range(iters):
     # The value to encode into the input column
-    valueToEncode = wave(t2) # Some wavy line
+    valueToEncode = wave(t) # Some wavy line
 
     #csdr = Unorm8ToCSDR(float(valueToEncode))
     csdr = [ int(valueToEncode * (h.getIOSize(0)[2] - 1) + 0.5) ]
 
     # Step the hierarchy given the inputs (just one here)
     h.step([ csdr ], True) # True for enabling learning
-
-    t2 += 1
-
-    if np.random.rand() < 0.02:
-        t2 += 1
 
     print(h.getHiddenCIs(0))
     # Print progress
