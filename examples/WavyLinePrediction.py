aed613a4852d221bf5670e3daa42bec2e9b72bdd
Eric Laukien <ericlaukien@gmail.com>
2022-08-21 20:21:55 -0400

    Impl

--- examples/WavyLinePrediction.py
+++ examples/WavyLinePrediction.py
@@ -34,7 +34,7 @@ inputColumnSize = 32
 # Define layer descriptors: Parameters of each layer upon creation
 lds = []
 
-for i in range(8): # LayFrs with exponential memory
+for i in range(6): # LayFrs with exponential memory
     ld = neo.LayerDesc()
 
     ld.hiddenSize = (4, 4, 32) # Size of the encoder (SparseCoder)
@@ -46,13 +46,10 @@ h = neo.Hierarchy()
 h.initRandom([ neo.IODesc(size=(1, numInputColumns, inputColumnSize), type=neo.prediction, dRadius=4) ], lds)
 
 # Present the wave sequence for some timesteps
-iters = 20000
+iters = 100000
 
 def wave(t):
-    if t % 20 == 0 or t % 7 == 0:
-        return 1.0
-    return 0.0
-    return min(1.0, max(0.0, (np.sin(t * 0.05 * 2.0 * np.pi + 0.5)) * np.sin(t * 0.04 * 2.0 * np.pi - 0.4) * 0.5 + 0.5 + np.random.randn() * 0.01))
+    return min(1.0, max(0.0, (np.sin(t * 0.05 * 2.0 * np.pi + 0.5)) * np.sin(t * 0.04 * 2.0 * np.pi - 0.4) * 0.5 + 0.5 + np.random.randn() * 0.02))
 
 for t in range(iters):
     # The value to encode into the input column
