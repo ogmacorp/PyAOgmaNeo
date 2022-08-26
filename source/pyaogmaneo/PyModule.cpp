71f94c9f3b4463ad323ba063a0e9fec689c29ef8
Eric Laukien <ericlaukien@gmail.com>
2022-08-26 16:40:28 -0400

    Tests

--- source/pyaogmaneo/PyModule.cpp
+++ source/pyaogmaneo/PyModule.cpp
@@ -61,7 +61,7 @@ PYBIND11_MODULE(pyaogmaneo, m) {
             py::arg("eRadius") = 2,
             py::arg("dRadius") = 2,
             py::arg("ticksPerUpdate") = 2,
-            py::arg("temporalHorizon") = 2
+            py::arg("temporalHorizon") = 4
         )
         .def_readwrite("hiddenSize", &pyaon::LayerDesc::hiddenSize)
         .def_readwrite("clumpSize", &pyaon::LayerDesc::clumpSize)
