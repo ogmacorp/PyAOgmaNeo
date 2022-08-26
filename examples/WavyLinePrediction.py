994ea809fa717ed2bdf1162a5d957ec4d4b1eb8d
Eric Laukien <ericlaukien@gmail.com>
2022-08-26 10:45:35 -0400

    Tuning

--- examples/WavyLinePrediction.py
+++ examples/WavyLinePrediction.py
@@ -100,12 +100,9 @@ h = neo.Hierarchy()
 h.initRandom([ neo.IODesc(size=(1, numInputColumns, inputColumnSize), type=neo.prediction) ], lds)
 
 # Present the (noisy) wave sequence for some timesteps
-iters = 10000
+iters = 20000
 
 def wave(t):
-    if t % 10 == 0:
-        return 1.0
-    return 0.0
     return min(1.0, max(0.0, (np.sin(t * 0.05 * 2.0 * np.pi + 0.5)) * np.sin(t * 0.04 * 2.0 * np.pi - 0.4) * 0.5 + 0.5 + np.random.randn() * 0.03))
 
 tNoisy = 0
@@ -119,9 +116,7 @@ for t in range(iters):
     # Step the hierarchy given the inputs (just one here)
     h.step([ csdr ], True) # True for enabling learning
 
-    print(h.getEHiddenCIs(0))
-
-    #print(h.getPredictionCIs(0))
+    print(h.getPredictionCIs(0))
 
     tNoisy += 1
 
