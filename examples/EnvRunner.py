25b807adf87e64c8080f813eac8b78e71c4f9b23
Eric Laukien <ericlaukien@gmail.com>
2022-08-29 12:38:12 -0400

    Tests

--- examples/EnvRunner.py
+++ examples/EnvRunner.py
@@ -23,7 +23,7 @@ inputTypePrediction = neo.prediction
 inputTypeAction = neo.action
 
 class EnvRunner:
-    def __init__(self, env, layerSizes=2 * [ (4, 4, 32) ], layerRadius=2, hiddenSize=(8, 8, 16), imageRadius=8, imageScale=1.0, obsResolution=64, actionResolution=9, rewardScale=1.0, terminalReward=0.0, infSensitivity=4.0, nThreads=4):
+    def __init__(self, env, layerSizes=1 * [ (4, 4, 32) ], layerRadius=2, hiddenSize=(8, 8, 16), imageRadius=8, imageScale=1.0, obsResolution=64, actionResolution=9, rewardScale=1.0, terminalReward=0.0, infSensitivity=4.0, nThreads=4):
         self.env = env
 
         neo.setNumThreads(nThreads)
@@ -203,9 +203,9 @@ class EnvRunner:
                 for j in range(len(self.inputLows[i])):
                     if self.inputLows[i][j] < self.inputHighs[i][j]:
                         # Rescale
-                        indices.append(int(min(1.0, max(0.0, (obs[j] - self.inputLows[i][j]) / (self.inputHighs[i][j] - self.inputLows[i][j]))) * (self.inputSizes[i][2] - 1) + 0.5))
-                        #v = obs[j]
-                        #indices.append(int(sigmoid(v * self.infSensitivity) * (self.inputSizes[i][2] - 1) + 0.5))
+                        #indices.append(int(min(1.0, max(0.0, (obs[j] - self.inputLows[i][j]) / (self.inputHighs[i][j] - self.inputLows[i][j]))) * (self.inputSizes[i][2] - 1) + 0.5))
+                        v = obs[j]
+                        indices.append(int(sigmoid(v * self.infSensitivity) * (self.inputSizes[i][2] - 1) + 0.5))
                     elif self.inputLows[i][j] > self.inputHighs[i][j]: # Inf
                         v = obs[j]
 
