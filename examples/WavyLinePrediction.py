d939181e0382dda4bd6983a252cd51768e762783
Eric Laukien <ericlaukien@gmail.com>
2022-09-01 21:34:09 -0400

    Testing

--- examples/WavyLinePrediction.py
+++ examples/WavyLinePrediction.py
@@ -91,7 +91,7 @@ lds = []
 for i in range(8): # Layers with exponential memory
     ld = neo.LayerDesc()
 
-    ld.hiddenSize = (4, 4, 32) # Size of the encoder(s) in the layer
+    ld.hiddenSize = (4, 4, 16) # Size of the encoder(s) in the layer
 
     lds.append(ld)
 
@@ -103,9 +103,6 @@ h.initRandom([ neo.IODesc(size=(1, numInputColumns, inputColumnSize), type=neo.p
 iters = 20000
 
 def wave(t):
-    if t % 100 == 0:
-        return 1.0
-    return 0.0
     return min(1.0, max(0.0, (np.sin(t * 0.05 * 2.0 * np.pi + 0.5)) * np.sin(t * 0.04 * 2.0 * np.pi - 0.4) * 0.5 + 0.5 + np.random.randn() * 0.03))
 
 tNoisy = 0
