bc3ae5fdb46d96bdffa27c41b82c7697b030551d
Eric Laukien <ericlaukien@gmail.com>
2022-09-06 16:43:07 -0400

    Vigilance

--- examples/WavyLinePrediction.py
+++ examples/WavyLinePrediction.py
@@ -103,7 +103,7 @@ h.initRandom([ neo.IODesc(size=(1, numInputColumns, inputColumnSize), type=neo.p
 iters = 100000
 
 def wave(t):
-    if t % 20 == 0:
+    if t % 50 == 0:
         return 1.0
     return 0.0
     return min(1.0, max(0.0, (np.sin(t * 0.05 * 2.0 * np.pi + 0.5)) * np.sin(t * 0.04 * 2.0 * np.pi - 0.4) * 0.5 + 0.5 + np.random.randn() * 0.03))
