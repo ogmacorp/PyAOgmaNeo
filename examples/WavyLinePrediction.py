a0f7790b269f35fc50321c7d7343e3a73234ce46
Eric Laukien <ericlaukien@gmail.com>
2022-09-10 13:50:49 -0400

    Tests

--- examples/WavyLinePrediction.py
+++ examples/WavyLinePrediction.py
@@ -103,7 +103,10 @@ h.initRandom([ neo.IODesc(size=(1, numInputColumns, inputColumnSize), type=neo.p
 iters = 100000
 
 def wave(t):
-    return min(1.0, max(0.0, (np.sin(t * 0.05 * 2.0 * np.pi + 0.5)) * np.sin(t * 0.04 * 2.0 * np.pi - 0.4) * 0.5 + 0.5 + np.random.randn() * 0.05))
+    if t % 20 == 0 or t % 7 == 0:
+        return 1.0
+    return 0.0
+    return min(1.0, max(0.0, (np.sin(t * 0.05 * 2.0 * np.pi + 0.5)) * np.sin(t * 0.04 * 2.0 * np.pi - 0.4) * 0.5 + 0.5 + np.random.randn() * 0.03))
 
 for t in range(iters):
     valueToEncode = wave(t)
