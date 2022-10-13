94fe9d493d6102507bb81035cd35883043f05c4d
Eric Laukien <ericlaukien@gmail.com>
2022-10-12 20:21:19 -0400

    Random feedback alignment

--- examples/WavyLinePrediction.py
+++ examples/WavyLinePrediction.py
@@ -103,7 +103,7 @@ h.initRandom([ neo.IODesc(size=(1, numInputColumns, inputColumnSize), type=neo.p
 iters = 20000
 
 def wave(t):
-    if t % 20 == 0 or t % 7 == 0:
+    if t % 20 == 0:
         return 1.0
     return 0.0
     return min(1.0, max(0.0, (np.sin(t * 0.05 * 2.0 * np.pi + 0.5)) * np.sin(t * 0.04 * 2.0 * np.pi - 0.4) * 0.5 + 0.5 + np.random.randn() * 0.05))
@@ -116,7 +116,7 @@ for t in range(iters):
     # Step the hierarchy given the inputs (just one here)
     h.step([ csdr ], True) # True for enabling learning
 
-    #print(h.getHiddenCIs(1))
+    print(h.getHiddenCIs(1))
 
     # Print progress
     if t % 100 == 0:
