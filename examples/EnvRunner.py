f59342d60c21c1c3913ddec0c900b405f7ac1a2c
Eric Laukien <ericlaukien@gmail.com>
2022-11-03 10:18:16 -0400

    Experiments 2

--- examples/EnvRunner.py
+++ examples/EnvRunner.py
@@ -23,7 +23,7 @@ inputTypePrediction = neo.prediction
 inputTypeAction = neo.action
 
 class EnvRunner:
-    def __init__(self, env, layerSizes=2 * [ (4, 4, 32) ], layerRadius=2, hiddenSize=(8, 8, 16), imageRadius=8, imageScale=1.0, obsResolution=32, actionResolution=9, rewardScale=1.0, terminalReward=0.0, infSensitivity=4.0, nThreads=4):
+    def __init__(self, env, layerSizes=2 * [ (4, 4, 32) ], layerRadius=2, hiddenSize=(8, 8, 16), imageRadius=8, imageScale=1.0, obsResolution=32, actionResolution=9, rewardScale=1.0, terminalReward=0.0, infSensitivity=3.0, nThreads=4):
         self.env = env
 
         neo.setNumThreads(nThreads)
