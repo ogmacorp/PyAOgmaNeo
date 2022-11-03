c7584a16656cd34d04f95eadeabf1a25934dc09b
Eric Laukien <ericlaukien@gmail.com>
2022-11-03 13:37:26 -0400

    Added location invariant

--- setup.py
+++ setup.py
@@ -40,6 +40,8 @@ class CMakeExtension(Extension):
             "source/pyaogmaneo/PyEncoder.cpp",
             "source/pyaogmaneo/PyImageEncoder.h",
             "source/pyaogmaneo/PyImageEncoder.cpp",
+            "source/pyaogmaneo/PyLocationInvariant.h",
+            "source/pyaogmaneo/PyLocationInvariant.cpp",
             "source/pyaogmaneo/PyModule.cpp",
             ])
 
