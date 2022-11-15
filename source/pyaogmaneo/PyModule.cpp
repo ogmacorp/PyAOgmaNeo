a548ba6caaaa8b90d22a6cea796eeb48e98f4b8f
Eric Laukien <ericlaukien@gmail.com>
2022-11-14 21:10:29 -0500

    Tuning

--- source/pyaogmaneo/PyModule.cpp
+++ source/pyaogmaneo/PyModule.cpp
@@ -59,7 +59,7 @@ PYBIND11_MODULE(pyaogmaneo, m) {
             py::arg("eRadius") = 2,
             py::arg("dRadius") = 2,
             py::arg("ticksPerUpdate") = 2,
-            py::arg("temporalHorizon") = 2
+            py::arg("temporalHorizon") = 4
         )
         .def_readwrite("hiddenSize", &pyaon::LayerDesc::hiddenSize)
         .def_readwrite("eRadius", &pyaon::LayerDesc::eRadius)
