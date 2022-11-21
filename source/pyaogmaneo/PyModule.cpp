d3c301f15d09b88145e3bd2a18e1f8f41563ce8b
Eric Laukien <ericlaukien@gmail.com>
2022-11-20 23:17:41 -0500

    Tests

--- source/pyaogmaneo/PyModule.cpp
+++ source/pyaogmaneo/PyModule.cpp
@@ -103,6 +103,8 @@ PYBIND11_MODULE(pyaogmaneo, m) {
         .def("getEGroupRadius", &pyaon::Hierarchy::getEGroupRadius)
         .def("setELR", &pyaon::Hierarchy::setELR)
         .def("getELR", &pyaon::Hierarchy::getELR)
+        .def("setDScale", &pyaon::Hierarchy::setDScale)
+        .def("getDScale", &pyaon::Hierarchy::getDScale)
         .def("setDLR", &pyaon::Hierarchy::setDLR)
         .def("getDLR", &pyaon::Hierarchy::getDLR)
         .def("setAVLR", &pyaon::Hierarchy::setAVLR)
