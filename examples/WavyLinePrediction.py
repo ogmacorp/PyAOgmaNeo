acd7916fbbbd87decbd0ef9b5fff3d1e5f298f62
Eric Laukien <ericlaukien@gmail.com>
2023-02-28 10:57:47 -0500

    Tuning

--- examples/WavyLinePrediction.py
+++ examples/WavyLinePrediction.py
@@ -99,7 +99,7 @@ for i in range(6): # Layers with exponential memory
 h = neo.Hierarchy([ neo.IODesc(size=(1, numInputColumns, inputColumnSize), type=neo.prediction) ], lds)
 
 # Present the wave sequence for some timesteps
-iters = 10000
+iters = 20000
 
 def wave(t):
     if t % 20 == 0 or t % 7 == 0:
