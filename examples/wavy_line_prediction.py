3dcad06d1ef6f41743c1bd6bc930f2ccf8058ff7
Eric Laukien <ericlaukien@gmail.com>
2023-03-26 14:18:57 -0400

    Experiments

--- examples/wavy_line_prediction.py
+++ examples/wavy_line_prediction.py
@@ -88,10 +88,10 @@ input_column_size = 16
 # define layer descriptors: parameters of each layer upon creation
 lds = []
 
-for i in range(8): # layers with exponential memory
+for i in range(6): # layers with exponential memory
     ld = neo.LayerDesc()
 
-    ld.hidden_size = (5, 5, 32) # size of the encoder(s) in the layer
+    ld.hidden_size = (4, 4, 32) # size of the encoder(s) in the layer
 
     lds.append(ld)
 
