eef6f573f52633d5e82421ea88e4b269abf0af7c
Eric Laukien <ericlaukien@gmail.com>
2023-04-09 16:03:22 -0400

    Tests

--- source/pyaogmaneo/py_module.cpp
+++ source/pyaogmaneo/py_module.cpp
@@ -68,13 +68,13 @@ PYBIND11_MODULE(pyaogmaneo, m) {
     py::class_<aon::Encoder::Params>(m, "EncoderParams")
         .def(py::init<>())
         .def_readwrite("choice", &aon::Encoder::Params::choice)
-        .def_readwrite("vigilance_low", &aon::Encoder::Params::vigilance_low)
-        .def_readwrite("vigilance_high", &aon::Encoder::Params::vigilance_high)
+        .def_readwrite("vigilance", &aon::Encoder::Params::vigilance)
         .def_readwrite("lr", &aon::Encoder::Params::lr)
         .def_readwrite("l_radius", &aon::Encoder::Params::l_radius);
 
     py::class_<aon::Decoder::Params>(m, "DecoderParams")
-        .def(py::init<>());
+        .def(py::init<>())
+        .def_readwrite("gap", &aon::Decoder::Params::gap);
 
     py::class_<aon::Actor::Params>(m, "Actorparams")
         .def(py::init<>())
