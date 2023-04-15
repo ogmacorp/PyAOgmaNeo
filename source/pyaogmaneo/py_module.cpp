8b816357a359c17b844922681feb5af1a5af5291
Eric Laukien <ericlaukien@gmail.com>
2023-04-14 22:42:11 -0400

    Experiments

--- source/pyaogmaneo/py_module.cpp
+++ source/pyaogmaneo/py_module.cpp
@@ -70,7 +70,8 @@ PYBIND11_MODULE(pyaogmaneo, m) {
     // bind params
     py::class_<aon::Encoder::Params>(m, "EncoderParams")
         .def(py::init<>())
-        .def_readwrite("radius", &aon::Encoder::Params::radius)
+        .def_readwrite("radius_local", &aon::Encoder::Params::radius_local)
+        .def_readwrite("radius_max", &aon::Encoder::Params::radius_max)
         .def_readwrite("lr", &aon::Encoder::Params::lr)
         .def_readwrite("l_radius", &aon::Encoder::Params::l_radius);
 
@@ -158,7 +159,8 @@ PYBIND11_MODULE(pyaogmaneo, m) {
     // bind params
     py::class_<aon::Image_Encoder::Params>(m, "ImageEncoderParams")
         .def(py::init<>())
-        .def_readwrite("radius", &aon::Image_Encoder::Params::radius)
+        .def_readwrite("radius_local", &aon::Image_Encoder::Params::radius_local)
+        .def_readwrite("radius_max", &aon::Image_Encoder::Params::radius_max)
         .def_readwrite("lr", &aon::Image_Encoder::Params::lr);
 
     py::class_<pyaon::Image_Encoder>(m, "ImageEncoder")
