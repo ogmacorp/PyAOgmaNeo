1b7807becfc435e92f4db03476336b8f57b968d7
Eric Laukien <ericlaukien@gmail.com>
2023-04-21 18:28:20 -0400

    Experiments

--- examples/wavy_line_prediction.py
+++ examples/wavy_line_prediction.py
@@ -102,7 +102,7 @@ h = neo.Hierarchy([ neo.IODesc(size=(1, num_input_columns, input_column_size), t
 iters = 20000
 
 def wave(t):
-    if t % 51 == 0:
+    if t % 71 == 0:
         return 1.0
     return 0.0
     return np.sin(t * 0.05 * 2.0 * np.pi + 0.5) * np.sin(t * 0.04 * 2.0 * np.pi - 0.4) * 0.5 + 0.5
