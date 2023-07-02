25c221f877660bffbde0bd92c04c36e1af7d3d69
Eric Laukien <ericlaukien@gmail.com>
2023-07-02 13:27:52 -0400

    Tests

--- examples/wavy_line_prediction.py
+++ examples/wavy_line_prediction.py
@@ -88,11 +88,10 @@ input_column_size = 16
 # define layer descriptors: parameters of each layer upon creation
 lds = []
 
-for i in range(3): # layers with exponential memory
+for i in range(5): # layers with exponential memory
     ld = neo.LayerDesc()
 
     ld.hidden_size = (5, 5, 64) # size of the encoder(s) in the layer
-    ld.recurrent_radius = 0
 
     lds.append(ld)
 
@@ -100,7 +99,7 @@ for i in range(3): # layers with exponential memory
 h = neo.Hierarchy([ neo.IODesc(size=(1, num_input_columns, input_column_size), io_type=neo.prediction) ], lds)
 
 # present the wave sequence for some timesteps
-iters = 30000
+iters = 50000
 
 def wave(t):
     if t % 50 == 0 or t % 7 == 0:
