ca7891e97fbeec4cf109a608c83435e4c1e12436
Eric Laukien <ericlaukien@gmail.com>
2023-07-19 22:10:00 -0400

    Tests

--- examples/wavy_line_prediction.py
+++ examples/wavy_line_prediction.py
@@ -92,7 +92,6 @@ for i in range(3): # layers with exponential memory
     ld = neo.LayerDesc()
 
     ld.hidden_size = (5, 5, 64) # size of the encoder(s) in the layer
-    ld.recurrent_radius = 2
 
     lds.append(ld)
 
