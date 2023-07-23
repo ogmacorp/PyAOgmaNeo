03a0f60b8ac861c4c2cb11dc3666d71997e6b290
Eric Laukien <ericlaukien@gmail.com>
2023-07-23 19:40:48 -0400

    Tests

--- source/pyaogmaneo/py_module.cpp
+++ source/pyaogmaneo/py_module.cpp
@@ -68,6 +68,7 @@ PYBIND11_MODULE(pyaogmaneo, m) {
     py::class_<aon::Encoder::Params>(m, "EncoderParams")
         .def(py::init<>())
         .def_readwrite("lr", &aon::Encoder::Params::lr)
+        .def_readwrite("br", &aon::Encoder::Params::br)
         .def_readwrite("gcurve", &aon::Encoder::Params::gcurve)
         .def_readwrite("recurrent_importance", &aon::Encoder::Params::recurrent_importance);
 
