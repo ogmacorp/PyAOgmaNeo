595820e57ee8a221c845061d8cc6322ed54f0889
Eric Laukien <ericlaukien@gmail.com>
2023-07-27 12:08:02 -0400

    Tests

--- source/pyaogmaneo/py_module.cpp
+++ source/pyaogmaneo/py_module.cpp
@@ -66,7 +66,9 @@ PYBIND11_MODULE(pyaogmaneo, m) {
 
     // bind params
     py::class_<aon::Encoder::Params>(m, "EncoderParams")
-        .def(py::init<>());
+        .def(py::init<>())
+        .def_readwrite("temperature", &aon::Encoder::Params::temperature)
+        .def_readwrite("lr", &aon::Encoder::Params::lr);
 
     py::class_<aon::Decoder::Params>(m, "DecoderParams")
         .def(py::init<>())
