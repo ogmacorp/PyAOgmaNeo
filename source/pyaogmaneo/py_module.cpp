2bff7adf184283f0cf51f97bf36cf7a32f31d9fc
Eric Laukien <ericlaukien@gmail.com>
2023-08-14 00:40:05 -0400

    Experiments

--- source/pyaogmaneo/py_module.cpp
+++ source/pyaogmaneo/py_module.cpp
@@ -74,7 +74,7 @@ PYBIND11_MODULE(pyaogmaneo, m) {
 
     py::class_<aon::Decoder::Params>(m, "DecoderParams")
         .def(py::init<>())
-        .def_readwrite("vigilance", &aon::Decoder::Params::vigilance)
+        .def_readwrite("scale", &aon::Decoder::Params::scale)
         .def_readwrite("lr", &aon::Decoder::Params::lr);
 
     py::class_<aon::Actor::Params>(m, "ActorParams")
