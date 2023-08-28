7461999e7e3c2f2b9057a5210536925cc10797dc
Eric Laukien <ericlaukien@gmail.com>
2023-08-28 12:27:03 -0400

    Tuning

--- examples/wavy_line_prediction.py
+++ examples/wavy_line_prediction.py
@@ -91,7 +91,7 @@ lds = []
 for i in range(3): # layers with exponential memory
     ld = neo.LayerDesc()
 
-    ld.hidden_size = (7, 7, 32) # size of the encoder(s) in the layer
+    ld.hidden_size = (5, 5, 32) # size of the encoder(s) in the layer
 
     lds.append(ld)
 
@@ -99,7 +99,7 @@ for i in range(3): # layers with exponential memory
 h = neo.Hierarchy([ neo.IODesc(size=(1, num_input_columns, input_column_size), io_type=neo.prediction) ], lds)
 
 # present the wave sequence for some timesteps
-iters = 20000
+iters = 50000
 
 def wave(t):
     if t % 50 == 0 or t % 7 == 0:
