9e4d83c6ebfbea9030444305054b7253bdb6af69
Eric Laukien <ericlaukien@gmail.com>
2023-08-29 18:21:03 -0400

    Experiments

--- source/pyaogmaneo/py_module.cpp
+++ source/pyaogmaneo/py_module.cpp
@@ -74,6 +74,7 @@ PYBIND11_MODULE(pyaogmaneo, m) {
 
     py::class_<aon::Decoder::Params>(m, "DecoderParams")
         .def(py::init<>())
+        .def_readwrite("remember", &aon::Decoder::Params::remember)
         .def_readwrite("forget", &aon::Decoder::Params::forget);
 
     py::class_<aon::Actor::Params>(m, "ActorParams")
