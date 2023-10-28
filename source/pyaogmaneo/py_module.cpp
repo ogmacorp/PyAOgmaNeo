efbe3f9b1c01f6527b0b934ea4f604f555acb291
Eric Laukien <ericlaukien@gmail.com>
2023-10-27 22:23:54 -0400

    Experiments to remove

--- source/pyaogmaneo/py_module.cpp
+++ source/pyaogmaneo/py_module.cpp
@@ -70,9 +70,10 @@ PYBIND11_MODULE(pyaogmaneo, m) {
     // bind params
     py::class_<aon::Encoder::Params>(m, "EncoderParams")
         .def(py::init<>())
-        .def_readwrite("scale", &aon::Encoder::Params::scale)
+        .def_readwrite("choice", &aon::Encoder::Params::choice)
+        .def_readwrite("vigilance", &aon::Encoder::Params::vigilance)
         .def_readwrite("lr", &aon::Encoder::Params::lr)
-        .def_readwrite("gcurve", &aon::Encoder::Params::gcurve);
+        .def_readwrite("l_radius", &aon::Encoder::Params::l_radius);
 
     py::class_<aon::Routed_Layer::Params>(m, "RoutedLayerParams")
         .def(py::init<>())
