e4fe9bb4ef8bde38ba063791b504a10759f94d96
Eric Laukien <ericlaukien@gmail.com>
2023-12-23 17:43:11 -0500

    2.3.0

--- source/pyaogmaneo/py_hierarchy.h
+++ source/pyaogmaneo/py_hierarchy.h
@@ -14,7 +14,7 @@
 namespace py = pybind11;
 
 namespace pyaon {
-const int hierarchy_magic = 7514621;
+const int hierarchy_magic = 2514123;
 
 enum IO_Type {
     none = 0,
--- source/pyaogmaneo/py_image_encoder.h
+++ source/pyaogmaneo/py_image_encoder.h
@@ -14,7 +14,7 @@
 namespace py = pybind11;
 
 namespace pyaon {
-const int image_encoder_magic = 3251108;
+const int image_encoder_magic = 5250129;
 
 struct Image_Visible_Layer_Desc {
     std::tuple<int, int, int> size;
