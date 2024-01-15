3eb2f59a81c62dca34a02d3ce774526cae7fd096
Eric Laukien <ericlaukien@gmail.com>
2024-01-14 19:20:55 -0500

    Q

--- source/pyaogmaneo/py_module.cpp
+++ source/pyaogmaneo/py_module.cpp
@@ -106,16 +106,15 @@ PYBIND11_MODULE(pyaogmaneo, m) {
     py::class_<aon::Decoder::Params>(m, "DecoderParams")
         .def(py::init<>())
         .def_readwrite("scale", &aon::Decoder::Params::scale)
-        .def_readwrite("lr", &aon::Decoder::Params::lr)
-        .def_readwrite("leak", &aon::Decoder::Params::leak);
+        .def_readwrite("lr", &aon::Decoder::Params::lr);
 
     py::class_<aon::Actor::Params>(m, "ActorParams")
         .def(py::init<>())
-        .def_readwrite("vlr", &aon::Actor::Params::vlr)
-        .def_readwrite("alr", &aon::Actor::Params::alr)
-        .def_readwrite("leak", &aon::Actor::Params::leak)
+        .def_readwrite("lr", &aon::Actor::Params::lr)
+        .def_readwrite("cons", &aon::Actor::Params::cons)
+        .def_readwrite("rate", &aon::Actor::Params::rate)
         .def_readwrite("discount", &aon::Actor::Params::discount)
-        .def_readwrite("min_steps", &aon::Actor::Params::min_steps)
+        .def_readwrite("n_steps", &aon::Actor::Params::n_steps)
         .def_readwrite("history_iters", &aon::Actor::Params::history_iters);
 
     py::class_<aon::Hierarchy::Layer_Params>(m, "LayerParams")
@@ -159,8 +158,7 @@ PYBIND11_MODULE(pyaogmaneo, m) {
         .def("step", &pyaon::Hierarchy::step,
             py::arg("input_cis"),
             py::arg("learn_enabled") = true,
-            py::arg("reward") = 0.0f,
-            py::arg("mimic") = 0.0f
+            py::arg("reward") = 0.0f
         )
         .def("clear_state", &pyaon::Hierarchy::clear_state)
         .def("get_num_layers", &pyaon::Hierarchy::get_num_layers)
