cf771f09fb41efd2c930d0bad84f5c6b28cf6cd1
Eric Laukien <ericlaukien@gmail.com>
2024-01-20 18:59:16 -0500

    2.3.10

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name="pyaogmaneo",
-    version="2.3.9",
+    version="2.3.10",
     description="Python bindings for the AOgmaNeo library",
     long_description='https://github.com/ogmacorp/PyAOgmaNeo',
     author='Ogma Intelligent Systems Corp',
