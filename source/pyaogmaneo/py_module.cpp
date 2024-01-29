1e0a1fc311b415b9a0b3c1aa38a92f2827f8878c
Eric Laukien <ericlaukien@gmail.com>
2024-01-29 11:33:55 -0500

    Experiments

--- source/pyaogmaneo/py_module.cpp
+++ source/pyaogmaneo/py_module.cpp
@@ -99,6 +99,7 @@ PYBIND11_MODULE(pyaogmaneo, m) {
     py::class_<aon::Encoder::Params>(m, "EncoderParams")
         .def(py::init<>())
         .def_readwrite("scale", &aon::Encoder::Params::scale)
+        .def_readwrite("threshold", &aon::Encoder::Params::threshold)
         .def_readwrite("lr", &aon::Encoder::Params::lr);
 
     py::class_<aon::Decoder::Params>(m, "DecoderParams")
