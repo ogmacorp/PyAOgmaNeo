ee450f1642f0e30013c70f94916cf09ae5043a45
Eric Laukien <ericlaukien@gmail.com>
2024-02-06 11:38:19 -0500

    Exponent

--- source/pyaogmaneo/py_module.cpp
+++ source/pyaogmaneo/py_module.cpp
@@ -98,8 +98,7 @@ PYBIND11_MODULE(pyaogmaneo, m) {
     // bind params
     py::class_<aon::Encoder::Params>(m, "EncoderParams")
         .def(py::init<>())
-        .def_readwrite("scale", &aon::Encoder::Params::scale)
-        .def_readwrite("threshold", &aon::Encoder::Params::threshold)
+        .def_readwrite("exponent", &aon::Encoder::Params::exponent)
         .def_readwrite("lr", &aon::Encoder::Params::lr);
 
     py::class_<aon::Decoder::Params>(m, "DecoderParams")
