504fdbc3d0d07a335890e2c16b757ee6a2488848
Eric Laukien <ericlaukien@gmail.com>
2024-04-01 00:28:27 -0400

    Tests

--- examples/env_runner.py
+++ examples/env_runner.py
@@ -23,9 +23,9 @@ input_type_prediction = neo.prediction
 input_type_action = neo.action
 
 class EnvRunner:
-    def __init__(self, env, layer_sizes=3 * [(5, 5, 32)],
-        num_dendrites_per_cell=8, input_radius=2, layer_radius=2, hidden_size=(10, 10, 16),
-        image_radius=8, image_scale=0.5, obs_resolution=16, action_resolution=15,
+    def __init__(self, env, layer_sizes=4 * [(5, 5, 64)],
+        num_dendrites_per_cell=4, input_radius=10, layer_radius=2, hidden_size=(10, 10, 16),
+        image_radius=8, image_scale=0.5, obs_resolution=8, action_resolution=9,
         reward_scale=1.0, terminal_reward=0.0, inf_sensitivity=4.0, n_threads=8
     ):
         self.env = env
@@ -226,8 +226,8 @@ class EnvRunner:
                 for j in range(len(self.input_lows[i])):
                     if self.input_lows[i][j] < self.input_highs[i][j]:
                         # rescale
-                        #indices.append(int(min(1.0, max(0.0, (obs[j] - self.input_lows[i][j]) / (self.input_highs[i][j] - self.input_lows[i][j]))) * (self.input_sizes[i][2] - 1) + 0.5))
-                        indices.append(int(sigmoid(obs[j] * self.inf_sensitivity) * (self.input_sizes[i][2] - 1) + 0.5))
+                        indices.append(int(min(1.0, max(0.0, (obs[j] - self.input_lows[i][j]) / (self.input_highs[i][j] - self.input_lows[i][j]))) * (self.input_sizes[i][2] - 1) + 0.5))
+                        #indices.append(int(sigmoid(obs[j] * self.inf_sensitivity) * (self.input_sizes[i][2] - 1) + 0.5))
                     elif self.input_lows[i][j] > self.input_highs[i][j]: # Inf
                         # Rescale
                         indices.append(int(sigmoid(obs[j] * self.inf_sensitivity) * (self.input_sizes[i][2] - 1) + 0.5))
