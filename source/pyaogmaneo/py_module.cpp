a0a1989a513a5db8eb1271e853d95de832252887
Eric Laukien <ericlaukien@gmail.com>
2024-04-01 13:31:12 -0400

    Return of ReLU dendrites

--- source/pyaogmaneo/py_module.cpp
+++ source/pyaogmaneo/py_module.cpp
@@ -102,12 +102,14 @@ PYBIND11_MODULE(pyaogmaneo, m) {
     py::class_<aon::Decoder::Params>(m, "DecoderParams")
         .def(py::init<>())
         .def_readwrite("scale", &aon::Decoder::Params::scale)
-        .def_readwrite("lr", &aon::Decoder::Params::lr);
+        .def_readwrite("lr", &aon::Decoder::Params::lr)
+        .def_readwrite("leak", &aon::Decoder::Params::leak);
 
     py::class_<aon::Actor::Params>(m, "ActorParams")
         .def(py::init<>())
         .def_readwrite("vlr", &aon::Actor::Params::vlr)
         .def_readwrite("plr", &aon::Actor::Params::plr)
+        .def_readwrite("leak", &aon::Actor::Params::leak)
         .def_readwrite("value_rate", &aon::Actor::Params::value_rate)
         .def_readwrite("discount", &aon::Actor::Params::discount)
         .def_readwrite("trace_curve", &aon::Actor::Params::trace_curve)
