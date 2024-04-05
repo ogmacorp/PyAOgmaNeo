9cca41cda0eca6ad3b3ab34cc6dc508df5cc15cf
Eric Laukien <ericlaukien@gmail.com>
2024-04-05 18:20:26 -0400

    Recurrent

--- source/pyaogmaneo/py_hierarchy.cpp
+++ source/pyaogmaneo/py_hierarchy.cpp
@@ -28,9 +28,6 @@ void IO_Desc::check_in_range() const {
 
     if (down_radius < 0)
         throw std::runtime_error("error: down_radius < 0 is not allowed!");
-
-    if (history_capacity < 2)
-        throw std::runtime_error("error: history_capacity < 2 is not allowed!");
 }
 
 void Layer_Desc::check_in_range() const {
@@ -109,8 +106,7 @@ void Hierarchy::init_random(
             static_cast<aon::IO_Type>(io_descs[i].type),
             io_descs[i].num_dendrites_per_cell,
             io_descs[i].up_radius,
-            io_descs[i].down_radius,
-            io_descs[i].history_capacity
+            io_descs[i].down_radius
         );
     }
     
