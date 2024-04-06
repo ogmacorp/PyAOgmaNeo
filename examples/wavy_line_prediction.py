622b90a507c45a8a9cfbd9d85a00974c9ad3a85f
Eric Laukien <ericlaukien@gmail.com>
2024-04-06 14:32:45 -0400

    Tests

--- examples/wavy_line_prediction.py
+++ examples/wavy_line_prediction.py
@@ -88,10 +88,10 @@ input_column_size = 16
 # define layer descriptors: parameters of each layer upon creation
 lds = []
 
-for i in range(1): # layers
+for i in range(3): # layers
     ld = neo.LayerDesc()
 
-    ld.hidden_size = (5, 5, 8) # size of the encoder(s) in the layer
+    ld.hidden_size = (5, 5, 16) # size of the encoder(s) in the layer
 
     lds.append(ld)
 
@@ -99,10 +99,13 @@ for i in range(1): # layers
 h = neo.Hierarchy([ neo.IODesc(size=(1, num_input_columns, input_column_size), io_type=neo.prediction) ], lds)
 
 # present the wave sequence for some timesteps, 1000 here
-iters = 700
+iters = 10000
 
 # function for the wave
 def wave(t):
+    if t % 50 == 0 or t % 7 == 0:
+        return 1.0
+    return 0.0
     return np.sin(t * 0.05 * 2.0 * np.pi + 0.5) * np.sin(t * 0.04 * 2.0 * np.pi - 0.4) * 0.5 + 0.5
 
 # iterate
