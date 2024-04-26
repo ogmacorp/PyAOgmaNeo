d87d6575ca914442f077fec23abd08c382cf05ff
Eric Laukien <ericlaukien@gmail.com>
2024-04-26 19:43:30 -0400

    Tests

--- examples/wavy_line_prediction.py
+++ examples/wavy_line_prediction.py
@@ -88,10 +88,11 @@ input_column_size = 16
 # define layer descriptors: parameters of each layer upon creation
 lds = []
 
-for i in range(6): # layers
+for i in range(1): # layers
     ld = neo.LayerDesc()
 
     ld.hidden_size = (5, 5, 64) # size of the encoder(s) in the layer
+    ld.recurrent_radius = 2
 
     lds.append(ld)
 
