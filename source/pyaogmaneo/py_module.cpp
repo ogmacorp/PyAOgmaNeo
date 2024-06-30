d286a44f913f48d46b6e6eacfc76a69d3f54affe
Eric Laukien <ericlaukien@gmail.com>
2024-06-30 19:01:48 -0400

    Actor clip

--- source/pyaogmaneo/py_module.cpp
+++ source/pyaogmaneo/py_module.cpp
@@ -113,7 +113,8 @@ PYBIND11_MODULE(pyaogmaneo, m) {
         .def_readwrite("vlr", &aon::Actor::Params::vlr)
         .def_readwrite("plr", &aon::Actor::Params::plr)
         .def_readwrite("discount", &aon::Actor::Params::discount)
-        .def_readwrite("trace_scale", &aon::Actor::Params::trace_scale)
+        .def_readwrite("value_clip", &aon::Actor::Params::value_clip)
+        .def_readwrite("policy_clip", &aon::Actor::Params::policy_clip)
         .def_readwrite("trace_decay", &aon::Actor::Params::trace_decay);
 
     py::class_<aon::Hierarchy::Layer_Params>(m, "LayerParams")
