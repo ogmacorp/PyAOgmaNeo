36d2cdeb830e33e1d4541c350df3905f9448dda7
Eric Laukien <ericlaukien@gmail.com>
2024-08-02 12:24:55 -0400

    Tests

--- examples/env_runner.py
+++ examples/env_runner.py
@@ -83,7 +83,7 @@ class EnvRunner:
 
         self.input_keys.append(key)
 
-    def __init__(self, env, layer_sizes=2 * [(5, 5, 32)],
+    def __init__(self, env, layer_sizes=4 * [(5, 5, 32)],
         num_dendrites_per_cell=4, value_num_dendrites_per_cell=16,
         input_radius=4, layer_radius=2, hidden_size=(10, 10, 16),
         image_radius=8, image_scale=0.25, obs_resolution=16, action_resolution=9, action_importance=0.3,
