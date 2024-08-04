ea49289deabbc3a5757abf94a3b82c714688c703
Eric Laukien <ericlaukien@gmail.com>
2024-08-04 10:41:59 -0400

    Confidence scaling

--- source/pyaogmaneo/py_module.cpp
+++ source/pyaogmaneo/py_module.cpp
@@ -118,6 +118,7 @@ PYBIND11_MODULE(pyaogmaneo, m) {
         .def_readwrite("leak", &aon::Actor::Params::leak)
         .def_readwrite("bias", &aon::Actor::Params::bias)
         .def_readwrite("discount", &aon::Actor::Params::discount)
+        .def_readwrite("confidence_scaling", &aon::Actor::Params::confidence_scaling)
         .def_readwrite("min_steps", &aon::Actor::Params::min_steps)
         .def_readwrite("history_iters", &aon::Actor::Params::history_iters);
 
