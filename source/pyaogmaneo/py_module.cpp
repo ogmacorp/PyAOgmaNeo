a0ecf62dbb6d90da89dfda7d0d1e0b80bd17eb26
Eric Laukien <ericlaukien@gmail.com>
2024-08-15 13:43:57 -0400

    Tests

--- source/pyaogmaneo/py_module.cpp
+++ source/pyaogmaneo/py_module.cpp
@@ -113,7 +113,7 @@ PYBIND11_MODULE(pyaogmaneo, m) {
         .def_readwrite("plr", &aon::Actor::Params::plr)
         .def_readwrite("leak", &aon::Actor::Params::leak)
         .def_readwrite("discount", &aon::Actor::Params::discount)
-        .def_readwrite("value_clip", &aon::Actor::Params::value_clip)
+        .def_readwrite("trace_squash", &aon::Actor::Params::trace_squash)
         .def_readwrite("trace_decay", &aon::Actor::Params::trace_decay);
 
     py::class_<aon::Hierarchy::Layer_Params>(m, "LayerParams")
