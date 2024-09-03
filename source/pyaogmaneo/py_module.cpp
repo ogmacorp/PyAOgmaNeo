451e287a61f2b4073262240c8298b7e2c5681040
Eric Laukien <ericlaukien@gmail.com>
2024-09-03 16:42:28 -0400

    Experiments

--- source/pyaogmaneo/py_module.cpp
+++ source/pyaogmaneo/py_module.cpp
@@ -101,10 +101,12 @@ PYBIND11_MODULE(pyaogmaneo, m) {
         .def(py::init<>())
         .def_readwrite("vlr", &aon::Actor::Params::vlr)
         .def_readwrite("plr", &aon::Actor::Params::plr)
+        .def_readwrite("leak", &aon::Actor::Params::leak)
+        .def_readwrite("smoothing", &aon::Actor::Params::smoothing)
         .def_readwrite("discount", &aon::Actor::Params::discount)
-        .def_readwrite("value_clip", &aon::Actor::Params::value_clip)
-        .def_readwrite("policy_clip", &aon::Actor::Params::policy_clip)
-        .def_readwrite("trace_decay", &aon::Actor::Params::trace_decay);
+        .def_readwrite("td_scale_decay", &aon::Actor::Params::td_scale_decay)
+        .def_readwrite("min_steps", &aon::Actor::Params::min_steps)
+        .def_readwrite("history_iters", &aon::Actor::Params::history_iters);
 
     py::class_<aon::Hierarchy::Layer_Params>(m, "LayerParams")
         .def(py::init<>())
