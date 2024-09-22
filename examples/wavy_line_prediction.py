620fbaed3e7b9279f4254454e075dda80ad95042
Eric Laukien <ericlaukien@gmail.com>
2024-09-22 00:34:39 -0400

    Tests

--- examples/wavy_line_prediction.py
+++ examples/wavy_line_prediction.py
@@ -38,7 +38,7 @@ for i in range(3):
 h = Hierarchy([ neo.IODesc(size=(1, 1), io_type=neo.prediction) ], lds)
 
 # present the wave sequence for some timesteps, 1000 here
-iters = 600
+iters = 1000
 
 # function for the wave
 def wave(t):
