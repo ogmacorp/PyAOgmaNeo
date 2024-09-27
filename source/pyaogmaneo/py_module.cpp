ce2f7ca2f975a0751409c6bf6e207843b4e6d051
Eric Laukien <ericlaukien@gmail.com>
2024-09-27 14:38:41 -0400

    Experiments

--- source/pyaogmaneo/py_module.cpp
+++ source/pyaogmaneo/py_module.cpp
@@ -115,6 +115,7 @@ PYBIND11_MODULE(pyaogmaneo, m) {
         .def_readwrite("plr", &aon::Actor::Params::plr)
         .def_readwrite("leak", &aon::Actor::Params::leak)
         .def_readwrite("smoothing", &aon::Actor::Params::smoothing)
+        .def_readwrite("bias", &aon::Actor::Params::bias)
         .def_readwrite("discount", &aon::Actor::Params::discount)
         .def_readwrite("td_scale_decay", &aon::Actor::Params::td_scale_decay)
         .def_readwrite("min_steps", &aon::Actor::Params::min_steps)
