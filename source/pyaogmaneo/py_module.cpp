1094a70e49d0f30947b4b60d4cf33a4efa6a84b8
Eric Laukien <ericlaukien@gmail.com>
2024-10-02 15:37:41 -0400

    Experiments

--- source/pyaogmaneo/py_module.cpp
+++ source/pyaogmaneo/py_module.cpp
@@ -101,6 +101,7 @@ PYBIND11_MODULE(pyaogmaneo, m) {
     // bind params
     py::class_<aon::Encoder::Params>(m, "EncoderParams")
         .def(py::init<>())
+        .def_readwrite("scale", &aon::Encoder::Params::scale)
         .def_readwrite("lr", &aon::Encoder::Params::lr)
         .def_readwrite("stability", &aon::Encoder::Params::stability);
 
