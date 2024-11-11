b8c4accb20c1ff77882e9e7284ea4ff8047c114a
Eric Laukien <ericlaukien@gmail.com>
2024-11-11 10:53:42 -0500

    2.7.29

--- source/pyaogmaneo/py_image_encoder.h
+++ source/pyaogmaneo/py_image_encoder.h
@@ -14,7 +14,7 @@
 namespace py = pybind11;
 
 namespace pyaon {
-const int image_encoder_magic = 2901131;
+const int image_encoder_magic = 3802131;
 
 struct Image_Visible_Layer_Desc {
     std::tuple<int, int, int> size;
