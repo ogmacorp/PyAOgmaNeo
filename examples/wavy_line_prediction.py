d57f6da40dfa38791cfec0c06dd134bf85d4ae14
Eric Laukien <ericlaukien@gmail.com>
2024-12-07 11:17:08 -0500

    2.7.32

--- examples/wavy_line_prediction.py
+++ examples/wavy_line_prediction.py
@@ -88,7 +88,7 @@ input_column_size = 16
 # define layer descriptors: parameters of each layer upon creation
 lds = []
 
-for i in range(4): # layers with exponential memory
+for i in range(2): # layers with exponential memory
     ld = neo.LayerDesc()
 
     ld.hidden_size = (5, 5, 16) # size of the encoder(s) in the layer
@@ -101,7 +101,7 @@ h = neo.Hierarchy([ neo.IODesc(size=(1, num_input_columns, input_column_size), i
 h.params.anticipation = True # Anticipation mode, faster learning of long sequences at the cost of some extra compute
 
 # present the wave sequence for some timesteps, 1000 here
-iters = 1000
+iters = 2000
 
 # function for the wave
 def wave(t):
--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name="pyaogmaneo",
-    version="2.7.31",
+    version="2.7.32",
     description="Python bindings for the AOgmaNeo library",
     long_description='https://github.com/ogmacorp/PyAOgmaNeo',
     author='Ogma Intelligent Systems Corp',
