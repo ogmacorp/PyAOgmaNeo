49ce1c0cd0fa8aa4d658b88f86f6440009eda99b
Eric Laukien <ericlaukien@gmail.com>
2024-12-16 15:41:30 -0500

    2.7.38

--- examples/wavy_line_prediction.py
+++ examples/wavy_line_prediction.py
@@ -91,7 +91,7 @@ lds = []
 for i in range(5): # layers with exponential memory
     ld = neo.LayerDesc()
 
-    ld.hidden_size = (5, 5, 32) # size of the encoder(s) in the layer
+    ld.hidden_size = (5, 5, 16) # size of the encoder(s) in the layer
 
     lds.append(ld)
 
