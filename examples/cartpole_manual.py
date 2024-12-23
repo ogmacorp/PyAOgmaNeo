f9f4586606d4068c845841f6b11e6a61c8a3f363
Eric Laukien <ericlaukien@gmail.com>
2024-12-23 17:27:24 -0500

    Experiments

--- examples/cartpole_manual.py
+++ examples/cartpole_manual.py
@@ -34,16 +34,22 @@ for i in range(2): # layers with exponential memory. Not much memory is needed f
     ld = neo.LayerDesc()
 
     # set some layer structural parameters
-    ld.hidden_size = (5, 5, 32)
+    ld.hidden_size = (5, 5, 64)
     
     lds.append(ld)
 
+s = neo.Searcher((5, 5, 32), 256)
+s.params.lr = 0.001
+s.params.exploration = 0.1
+
 # create the hierarchy
-h = neo.Hierarchy([ neo.IODesc((2, 2, input_resolution), neo.none), neo.IODesc((1, 1, num_actions), neo.action) ], lds)
+h = neo.Hierarchy([ neo.IODesc((2, 2, input_resolution), neo.none), neo.IODesc((1, 1, num_actions), neo.prediction) ], lds)
 
 reward = 0.0
+average_reward = 0.0
+action = 0
 
-for episode in range(1000):
+for episode in range(10000):
     obs, _ = env.reset()
 
     # timesteps
@@ -51,10 +57,17 @@ for episode in range(1000):
         # sensory CSDR creation through "squash and bin" method
         csdr = (sigmoid(obs * 3.0) * (input_resolution - 1) + 0.5).astype(np.int32)
 
-        h.step([ csdr, h.get_prediction_cis(1) ], True, reward)
+        average_reward += 0.001 * (reward - average_reward)
+
+        s.step(average_reward, True)
+
+        h.step([ csdr, [ action ] ], s.get_config_cis(), True)
 
         # retrieve the action, the hierarchy already automatically applied exploration
         action = h.get_prediction_cis(1)[0] # First and only column
+
+        if np.random.rand() < 0.05:
+            action = np.random.randint(0, num_actions)
 
         obs, reward, term, trunc, _ = env.step(action)
 
@@ -65,6 +78,6 @@ for episode in range(1000):
             reward = 0.0
 
         if term or trunc:
-            print(f"Episode {episode + 1} finished after {t + 1} timesteps")
+            print(f"Episode {episode + 1} finished after {t + 1} timesteps, {average_reward} average reward.")
 
             break
