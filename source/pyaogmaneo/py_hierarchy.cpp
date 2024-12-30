98796d57bb21ff638473a615afade76195ddfba2
Eric Laukien <ericlaukien@gmail.com>
2024-12-30 15:07:47 -0500

    Traces

--- source/pyaogmaneo/py_hierarchy.cpp
+++ source/pyaogmaneo/py_hierarchy.cpp
@@ -109,8 +109,7 @@ void Hierarchy::init_random(
             io_descs[i].num_dendrites_per_cell,
             io_descs[i].value_num_dendrites_per_cell,
             io_descs[i].up_radius,
-            io_descs[i].down_radius,
-            io_descs[i].history_capacity
+            io_descs[i].down_radius
         );
     }
     
