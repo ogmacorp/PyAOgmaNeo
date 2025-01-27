bb45fd698ce6bb5823f8be09b6ab254a0aabb852
Eric Laukien <ericlaukien@gmail.com>
2025-01-27 11:35:50 -0500

    Tests with stability

--- source/pyaogmaneo/py_module.cpp
+++ source/pyaogmaneo/py_module.cpp
@@ -95,17 +95,17 @@ PYBIND11_MODULE(pyaogmaneo, m) {
     // bind params
     py::class_<aon::Encoder::Params>(m, "EncoderParams")
         .def(py::init<>())
-        .def_readwrite("choice", &aon::Encoder::Params::choice)
-        .def_readwrite("mismatch", &aon::Encoder::Params::mismatch)
+        .def_readwrite("scale", &aon::Encoder::Params::scale)
         .def_readwrite("lr", &aon::Encoder::Params::lr)
-        .def_readwrite("active_ratio", &aon::Encoder::Params::active_ratio)
-        .def_readwrite("l_radius", &aon::Encoder::Params::l_radius);
+        .def_readwrite("stability", &aon::Encoder::Params::stability)
+        .def_readwrite("early_stop_cells", &aon::Encoder::Params::early_stop_cells);
 
     py::class_<aon::Decoder::Params>(m, "DecoderParams")
         .def(py::init<>())
         .def_readwrite("scale", &aon::Decoder::Params::scale)
         .def_readwrite("lr", &aon::Decoder::Params::lr)
-        .def_readwrite("leak", &aon::Decoder::Params::leak);
+        .def_readwrite("leak", &aon::Decoder::Params::leak)
+        .def_readwrite("stability", &aon::Decoder::Params::stability);
 
     py::class_<aon::Actor::Params>(m, "ActorParams")
         .def(py::init<>())
