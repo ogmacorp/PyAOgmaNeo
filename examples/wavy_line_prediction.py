a944f193e113ee27dc5b6bf671ff51fecf56df1e
Eric Laukien <ericlaukien@gmail.com>
2025-02-01 16:32:51 -0500

    2.10.0

--- examples/wavy_line_prediction.py
+++ examples/wavy_line_prediction.py
@@ -88,11 +88,11 @@ input_column_size = 16
 # define layer descriptors: parameters of each layer upon creation
 lds = []
 
-for i in range(2): # layers with exponential memory
+for i in range(2): # layers
     ld = neo.LayerDesc()
 
     ld.hidden_size = (5, 5, 16) # size of the encoder(s) in the layer
-    ld.temporal_size = 4
+    ld.temporal_size = 4 # amount of memory cells per spatial cell
 
     lds.append(ld)
 
@@ -100,13 +100,10 @@ for i in range(2): # layers with exponential memory
 h = neo.Hierarchy([ neo.IODesc(size=(1, num_input_columns, input_column_size), io_type=neo.prediction) ], lds)
 
 # present the wave sequence for some timesteps, 1000 here
-iters = 10000
+iters = 1000
 
 # function for the wave
 def wave(t):
-    if t % 20 == 0 or t % 7 == 0:
-        return 1.0
-    return 0.0
     return np.sin(t * 0.05 * 2.0 * np.pi + 0.5) * np.sin(t * 0.04 * 2.0 * np.pi - 0.4) * 0.5 + 0.5
 
 # iterate
--- source/pyaogmaneo/py_hierarchy.h
+++ source/pyaogmaneo/py_hierarchy.h
@@ -14,7 +14,7 @@
 namespace py = pybind11;
 
 namespace pyaon {
-const int hierarchy_magic = 8942905;
+const int hierarchy_magic = 2119905;
 
 enum IO_Type {
     none = 0,
