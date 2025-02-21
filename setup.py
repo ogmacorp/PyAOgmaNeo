942f5bab0250ed6a5c018c21f4038181027caaf0
Eric Laukien <ericlaukien@gmail.com>
2025-02-21 12:42:10 -0500

    2.10.7

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name="pyaogmaneo",
-    version="2.10.6",
+    version="2.10.7",
     description="Python bindings for the AOgmaNeo library",
     long_description='https://github.com/ogmacorp/PyAOgmaNeo',
     author='Ogma Intelligent Systems Corp',
--- source/pyaogmaneo/py_hierarchy.h
+++ source/pyaogmaneo/py_hierarchy.h
@@ -14,7 +14,7 @@
 namespace py = pybind11;
 
 namespace pyaon {
-const int hierarchy_magic = 5712937;
+const int hierarchy_magic = 7211917;
 
 enum IO_Type {
     none = 0,
