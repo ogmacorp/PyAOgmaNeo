413eaabe901b939893d501a5ceb4ee22583c91c6
Eric Laukien <ericlaukien@gmail.com>
2025-03-31 23:36:51 -0400

    2.13.2

--- examples/wavy_line_prediction.py
+++ examples/wavy_line_prediction.py
@@ -88,10 +88,10 @@ input_column_size = 16
 # define layer descriptors: parameters of each layer upon creation
 lds = []
 
-for i in range(5): # layers
+for i in range(1): # layers
     ld = neo.LayerDesc()
 
-    ld.hidden_size = (5, 5, 32) # size of the encoder(s) in the layer
+    ld.hidden_size = (5, 5, 16) # size of the encoder(s) in the layer
 
     lds.append(ld)
 
@@ -99,13 +99,10 @@ for i in range(5): # layers
 h = neo.Hierarchy([ neo.IODesc(size=(1, num_input_columns, input_column_size), io_type=neo.prediction) ], lds)
 
 # present the wave sequence for some timesteps, 1000 here
-iters = 10000
+iters = 5000
 
 # function for the wave
 def wave(t):
-    if t % 30 == 0 or t % 7 == 0:
-        return 1.0
-    return 0.0
     return np.sin(t * 0.05 * 2.0 * np.pi + 0.5) * np.sin(t * 0.04 * 2.0 * np.pi - 0.4) * 0.5 + 0.5
 
 # iterate
@@ -118,7 +115,7 @@ for t in range(iters):
     # step the hierarchy given the inputs (just one here)
     h.step([ csdr ], True) # true for enabling learning
 
-    print(h.get_hidden_cis(2))
+    #print(h.get_hidden_cis(2))
 
     # print progress
     if t % 100 == 0:
--- source/pyaogmaneo/py_hierarchy.h
+++ source/pyaogmaneo/py_hierarchy.h
@@ -14,7 +14,7 @@
 namespace py = pybind11;
 
 namespace pyaon {
-const int hierarchy_magic = 3310217;
+const int hierarchy_magic = 4720517;
 
 enum IO_Type {
     none = 0,
