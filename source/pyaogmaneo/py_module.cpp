8fea9f5de2a78827e4b2ff673715b9962f3dc7f4
Eric Laukien <ericlaukien@gmail.com>
2025-04-09 20:26:28 -0400

    2.13.4

--- source/pyaogmaneo/py_module.cpp
+++ source/pyaogmaneo/py_module.cpp
@@ -98,12 +98,9 @@ PYBIND11_MODULE(pyaogmaneo, m) {
     // bind params
     py::class_<aon::Encoder::Params>(m, "EncoderParams")
         .def(py::init<>())
-        .def_readwrite("falloff", &aon::Encoder::Params::falloff)
-        .def_readwrite("choice", &aon::Encoder::Params::choice)
+        .def_readwrite("scale", &aon::Encoder::Params::scale)
         .def_readwrite("lr", &aon::Encoder::Params::lr)
-        .def_readwrite("active_ratio", &aon::Encoder::Params::active_ratio)
-        .def_readwrite("l_radius", &aon::Encoder::Params::l_radius)
-        .def_readwrite("n_radius", &aon::Encoder::Params::n_radius);
+        .def_readwrite("early_stop", &aon::Encoder::Params::early_stop);
 
     py::class_<aon::Decoder::Params>(m, "DecoderParams")
         .def(py::init<>())
@@ -213,7 +210,7 @@ PYBIND11_MODULE(pyaogmaneo, m) {
         .def_readwrite("lr", &aon::Image_Encoder::Params::lr)
         .def_readwrite("scale", &aon::Image_Encoder::Params::scale)
         .def_readwrite("rr", &aon::Image_Encoder::Params::rr)
-        .def_readwrite("n_radius", &aon::Image_Encoder::Params::n_radius);
+        .def_readwrite("radius", &aon::Image_Encoder::Params::radius);
 
     py::class_<pyaon::Image_Encoder>(m, "ImageEncoder")
         .def(py::init<
