bee6e3ec7997a5f1543066c839ea3cf218478a4c
Eric Laukien <ericlaukien@gmail.com>
2025-04-12 19:33:40 -0400

    Tuning

--- examples/wavy_line_prediction.py
+++ examples/wavy_line_prediction.py
@@ -88,11 +88,10 @@ input_column_size = 16
 # define layer descriptors: parameters of each layer upon creation
 lds = []
 
-for i in range(4): # layers
+for i in range(1): # layers
     ld = neo.LayerDesc()
 
     ld.hidden_size = (5, 5, 32) # size of the encoder(s) in the layer
-    ld.recurrent_radius = 0
     #ld.temporal_size = 8
 
     lds.append(ld)
@@ -105,7 +104,7 @@ iters = 10000
 
 # function for the wave
 def wave(t):
-    if t % 30 == 0 or t % 7 == 0:
+    if t % 50 == 0 or t % 7 == 0:
         return 1.0
     return 0.0
     return np.sin(t * 0.05 * 2.0 * np.pi + 0.5) * np.sin(t * 0.04 * 2.0 * np.pi - 0.4) * 0.5 + 0.5
