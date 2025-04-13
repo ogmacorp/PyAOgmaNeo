eec0bb28fed3406ef13cf7ac8b52e1acef69e7f1
Eric Laukien <ericlaukien@gmail.com>
2025-04-12 21:50:21 -0400

    Tuning

--- source/pyaogmaneo/py_module.cpp
+++ source/pyaogmaneo/py_module.cpp
@@ -99,10 +99,9 @@ PYBIND11_MODULE(pyaogmaneo, m) {
     py::class_<aon::Encoder::Params>(m, "EncoderParams")
         .def(py::init<>())
         .def_readwrite("choice", &aon::Encoder::Params::choice)
-        .def_readwrite("category_vigilance", &aon::Encoder::Params::category_vigilance)
-        .def_readwrite("compare_vigilance", &aon::Encoder::Params::compare_vigilance)
+        .def_readwrite("vigilance", &aon::Encoder::Params::vigilance)
         .def_readwrite("lr", &aon::Encoder::Params::lr)
-        .def_readwrite("active", &aon::Encoder::Params::active_ratio)
+        .def_readwrite("active_ratio", &aon::Encoder::Params::active_ratio)
         .def_readwrite("l_radius", &aon::Encoder::Params::l_radius);
 
     py::class_<aon::Decoder::Params>(m, "DecoderParams")
