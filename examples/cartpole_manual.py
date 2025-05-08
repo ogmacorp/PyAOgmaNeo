c58770a563b0bfbbd7e7ce19511b94e8c62b890e
Eric Laukien <ericlaukien@gmail.com>
2025-05-07 23:20:07 -0400

    UDRL

--- examples/cartpole_manual.py
+++ examples/cartpole_manual.py
@@ -13,6 +13,36 @@ import gymnasium as gym
 import numpy as np
 import struct
 
+# multi-scale embedding
+def f_to_csdr(x, num_columns, cells_per_column, scale_factor=0.25):
+    csdr = []
+
+    scale = 1.0
+
+    for i in range(num_columns):
+        s = (x / scale) % (1.0 if x > 0.0 else -1.0)
+
+        csdr.append(int((s * 0.5 + 0.5) * (cells_per_column - 1) + 0.5))
+
+        rec = scale * (float(csdr[i]) / float(cells_per_column - 1) * 2.0 - 1.0)
+        x -= rec
+
+        scale *= scale_factor
+
+    return np.array(csdr, dtype=np.int32)
+
+def csdr_to_f(csdr, cells_per_column, scale_factor=0.25):
+    x = 0.0
+
+    scale = 1.0
+
+    for i in range(len(csdr)):
+        x += scale * (float(csdr[i]) / float(cells_per_column - 1) * 2.0 - 1.0)
+
+        scale *= scale_factor
+
+    return x
+
 # convert an ieee float to 8 columns with 16 cells each (similar to first approach but on floating-point data)
 def ieee_to_csdr(x : float):
     b = struct.pack("<f", x)
@@ -43,7 +73,7 @@ env = gym.make('CartPole-v1')
 # get observation size
 num_obs = env.observation_space.shape[0] # 4 values for Cart-Pole
 num_actions = env.action_space.n # N actions (1 discrete value)
-input_resolution = 32
+input_resolution = 16
 
 # set the number of threads
 neo.set_num_threads(4)
@@ -63,12 +93,12 @@ for i in range(1): # layers with exponential memory. Not much memory is needed f
 h = neo.Hierarchy([ neo.IODesc((2, 2, input_resolution), neo.none), neo.IODesc((1, 1, num_actions), neo.prediction), neo.IODesc((2, 4, 16), neo.prediction) ], lds)
 
 input_history = []
-max_history = 1000
+max_history = 500
 action = 0
 reward = 0.0
 future_state = h.serialize_state_to_buffer()
 reward_bump = 0.1
-exploration = 0.02
+exploration = 0.04
 
 for episode in range(10000):
     obs, _ = env.reset()
@@ -87,7 +117,7 @@ for episode in range(10000):
             average_reward = 0.0
 
             for i in range(max_history):
-                average_reward += input_history[i][2] * pow(0.95, i)
+                average_reward += input_history[i][2] * pow(0.97, i)
 
             average_reward /= max_history
 
