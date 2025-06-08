6615288e0e17d61d4c91ffe0b9177b7f86f053d6
Eric Laukien <ericlaukien@gmail.com>
2025-06-08 05:30:19 -0400

    Traced AWAC

--- source/pyaogmaneo/py_hierarchy.h
+++ source/pyaogmaneo/py_hierarchy.h
@@ -14,7 +14,7 @@
 namespace py = pybind11;
 
 namespace pyaon {
-const int hierarchy_magic = 4720517;
+const int hierarchy_magic = 2822577;
 
 enum IO_Type {
     none = 0,
@@ -27,30 +27,23 @@ struct IO_Desc {
     IO_Type type;
 
     int num_dendrites_per_cell;
-    int value_num_dendrites_per_cell;
 
     int up_radius;
     int down_radius;
-
-    int history_capacity;
 
     IO_Desc(
         const std::tuple<int, int, int> &size,
         IO_Type type,
         int num_dendrites_per_cell,
-        int value_num_dendrites_per_cell,
         int up_radius,
-        int down_radius,
-        int history_capacity
+        int down_radius
     )
     :
     size(size),
     type(type),
     num_dendrites_per_cell(num_dendrites_per_cell),
-    value_num_dendrites_per_cell(value_num_dendrites_per_cell),
     up_radius(up_radius),
-    down_radius(down_radius),
-    history_capacity(history_capacity)
+    down_radius(down_radius)
     {}
 
     void check_in_range() const;
@@ -155,8 +148,7 @@ public:
     void step(
         const std::vector<py::array_t<int, py::array::c_style | py::array::forcecast>> &input_cis,
         bool learn_enabled,
-        float reward,
-        float mimic
+        float reward
     );
 
     void clear_state() {
