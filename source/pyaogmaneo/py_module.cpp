56110bad53d89ff31cb5ea11bec1319798b69376
Eric Laukien <ericlaukien@gmail.com>
2025-06-08 05:41:58 -0400

    Traced AWAC with clip

--- source/pyaogmaneo/py_module.cpp
+++ source/pyaogmaneo/py_module.cpp
@@ -106,6 +106,7 @@ PYBIND11_MODULE(pyaogmaneo, m) {
         .def_readwrite("plr", &aon::Actor::Params::plr)
         .def_readwrite("discount", &aon::Actor::Params::discount)
         .def_readwrite("trace_decay", &aon::Actor::Params::trace_decay)
+        .def_readwrite("reweight", &aon::Actor::Params::reweight)
         .def_readwrite("td_clip", &aon::Actor::Params::td_clip);
 
     py::class_<aon::Hierarchy::Layer_Params>(m, "LayerParams")
