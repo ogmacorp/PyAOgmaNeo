1ab37a8eee968c50980fb6d84e3d9a3f0cd836ad
Eric Laukien <ericlaukien@gmail.com>
2025-06-08 07:29:28 -0400

    Traced AWAC with clip

--- source/pyaogmaneo/py_module.cpp
+++ source/pyaogmaneo/py_module.cpp
@@ -96,8 +96,7 @@ PYBIND11_MODULE(pyaogmaneo, m) {
     py::class_<aon::Encoder::Params>(m, "EncoderParams")
         .def(py::init<>())
         .def_readwrite("scale", &aon::Encoder::Params::scale)
-        .def_readwrite("lr", &aon::Encoder::Params::lr)
-        .def_readwrite("early_stop", &aon::Encoder::Params::early_stop);
+        .def_readwrite("lr", &aon::Encoder::Params::lr);
 
     py::class_<aon::Decoder::Params>(m, "DecoderParams")
         .def(py::init<>())
