efb3ef8a62ef4c12b98e368a99cfc4e4f72b4062
Eric Laukien <ericlaukien@gmail.com>
2025-06-17 06:58:27 -0400

    Example update

--- examples/cartpole_env_runner.py
+++ examples/cartpole_env_runner.py
@@ -20,11 +20,9 @@ runner = EnvRunner(env, terminal_reward=-100.0, reward_scale=0.0) # Cart-Pole en
 for episode in range(10000):
     env.reset()
 
-    runner.learn_enabled = episode < 1000
-
     # Timesteps
     for t in range(500):
-        done, _ = runner.act(epsilon=float(runner.learn_enabled)) # Step the environment and agent
+        done, _ = runner.act() # Step the environment and agent
 
         if done:
             print(f"Episode {episode + 1} finished after {t + 1} timesteps")
