121ef1a26bfcbbfbfb519bc4bab9280063fc3c7f
Eric Laukien <ericlaukien@gmail.com>
2025-06-19 23:44:36 -0400

    Smoother SOM

--- source/pyaogmaneo/py_module.cpp
+++ source/pyaogmaneo/py_module.cpp
@@ -99,6 +99,7 @@ PYBIND11_MODULE(pyaogmaneo, m) {
     py::class_<aon::Encoder::Params>(m, "EncoderParams")
         .def(py::init<>())
         .def_readwrite("falloff", &aon::Encoder::Params::falloff)
+        .def_readwrite("min_rate", &aon::Encoder::Params::min_rate)
         .def_readwrite("choice", &aon::Encoder::Params::choice)
         .def_readwrite("lr", &aon::Encoder::Params::lr)
         .def_readwrite("active_ratio", &aon::Encoder::Params::active_ratio)
@@ -209,6 +210,7 @@ PYBIND11_MODULE(pyaogmaneo, m) {
     py::class_<aon::Image_Encoder::Params>(m, "ImageEncoderParams")
         .def(py::init<>())
         .def_readwrite("falloff", &aon::Image_Encoder::Params::falloff)
+        .def_readwrite("min_rate", &aon::Image_Encoder::Params::min_rate)
         .def_readwrite("lr", &aon::Image_Encoder::Params::lr)
         .def_readwrite("scale", &aon::Image_Encoder::Params::scale)
         .def_readwrite("rr", &aon::Image_Encoder::Params::rr);
