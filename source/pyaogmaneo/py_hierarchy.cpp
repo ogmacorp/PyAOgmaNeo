0dd16d77aea6cdcab02f521b0fcfcc873c635175
Eric Laukien <ericlaukien@gmail.com>
2025-06-20 03:02:37 -0400

    Older enc

--- source/pyaogmaneo/py_hierarchy.cpp
+++ source/pyaogmaneo/py_hierarchy.cpp
@@ -444,14 +444,18 @@ std::tuple<py::array_t<unsigned char>, std::tuple<int, int, int>> Hierarchy::get
     for (int i = 0; i < field_count; i++)
         view(i) = 0;
 
+    int hidden_cell_index = std::get<2>(pos) + hidden_cells_start;
+
     for (int ix = iter_lower_bound.x; ix <= iter_upper_bound.x; ix++)
         for (int iy = iter_lower_bound.y; iy <= iter_upper_bound.y; iy++) {
             int visible_column_index = address2(aon::Int2(ix, iy), aon::Int2(vld.size.x, vld.size.y));
 
             aon::Int2 offset(ix - field_lower_bound.x, iy - field_lower_bound.y);
 
+            int wi_start = vld.size.z * (offset.y + diam * (offset.x + diam * hidden_cell_index));
+
             for (int vc = 0; vc < vld.size.z; vc++) {
-                int wi = std::get<2>(pos) + hidden_size.z * (offset.y + diam * (offset.x + diam * (vc + vld.size.z * hidden_column_index)));
+                int wi = vc + wi_start;
 
                 view(vc + vld.size.z * (offset.y + diam * offset.x)) = vl.weights[wi];
             }
