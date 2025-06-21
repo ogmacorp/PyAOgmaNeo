a74501aa1ae97b27e07c4effc28b778703322372
Eric Laukien <ericlaukien@gmail.com>
2025-06-21 08:08:03 -0400

    Tuning

--- examples/wavy_line_prediction.py
+++ examples/wavy_line_prediction.py
@@ -102,7 +102,7 @@ for i in range(3): # layers
 h = neo.Hierarchy([ neo.IODesc(size=(1, num_input_columns, input_column_size), io_type=neo.action) ], lds)
 
 # present the wave sequence for some timesteps, 1000 here
-iters = 10000
+iters = 1000
 
 # function for the wave
 def wave(t):
