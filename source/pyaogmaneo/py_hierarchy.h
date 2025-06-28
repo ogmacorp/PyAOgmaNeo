816faeda4d8e73bc7332efcd24cf965d7920be43
Eric Laukien <ericlaukien@gmail.com>
2025-06-27 22:28:29 -0400

    Tests

--- source/pyaogmaneo/py_hierarchy.h
+++ source/pyaogmaneo/py_hierarchy.h
@@ -14,7 +14,7 @@
 namespace py = pybind11;
 
 namespace pyaon {
-const int hierarchy_magic = 8740512;
+const int hierarchy_magic = 4720517;
 
 enum IO_Type {
     none = 0,
@@ -27,7 +27,6 @@ struct IO_Desc {
     IO_Type type;
 
     int num_dendrites_per_cell;
-    int value_num_dendrites_per_cell;
 
     int up_radius;
     int down_radius;
@@ -38,7 +37,6 @@ struct IO_Desc {
         const std::tuple<int, int, int> &size,
         IO_Type type,
         int num_dendrites_per_cell,
-        int value_num_dendrites_per_cell,
         int up_radius,
         int down_radius,
         int history_capacity
@@ -47,7 +45,6 @@ struct IO_Desc {
     size(size),
     type(type),
     num_dendrites_per_cell(num_dendrites_per_cell),
-    value_num_dendrites_per_cell(value_num_dendrites_per_cell),
     up_radius(up_radius),
     down_radius(down_radius),
     history_capacity(history_capacity)
@@ -155,8 +152,7 @@ public:
     void step(
         const std::vector<py::array_t<int, py::array::c_style | py::array::forcecast>> &input_cis,
         bool learn_enabled,
-        float reward,
-        float mimic
+        float reward
     );
 
     void clear_state() {
