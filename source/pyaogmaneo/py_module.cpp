03115cfebc992a71bdf5407aa382e0fe5f2e0cc6
Eric Laukien <ericlaukien@gmail.com>
2025-07-08 07:50:01 -0400

    Tests

--- source/pyaogmaneo/py_module.cpp
+++ source/pyaogmaneo/py_module.cpp
@@ -94,8 +94,7 @@ PYBIND11_MODULE(pyaogmaneo, m) {
     py::class_<aon::Encoder::Params>(m, "EncoderParams")
         .def(py::init<>())
         .def_readwrite("choice", &aon::Encoder::Params::choice)
-        .def_readwrite("vigilance_local", &aon::Encoder::Params::vigilance_local)
-        .def_readwrite("vigilance_global", &aon::Encoder::Params::vigilance_global)
+        .def_readwrite("vigilance", &aon::Encoder::Params::vigilance)
         .def_readwrite("lr", &aon::Encoder::Params::lr)
         .def_readwrite("active_ratio", &aon::Encoder::Params::active_ratio)
         .def_readwrite("l_radius", &aon::Encoder::Params::l_radius);
